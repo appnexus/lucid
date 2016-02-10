c7a6d54d0cbb81c286e188cfe5a0e298daf1f933
Dan Hahn <dhahn@appnexus.com>
2016-02-10 16:51:37 -0500

    Add new button styles

--- src/components/Button/examples/basic.jsx
+++ src/components/Button/examples/basic.jsx
@@ -7,7 +7,11 @@ export default React.createClass({
 			<div>
 				<Button>Plain</Button>
 				<Button className='BertButton-primary'>Primary</Button>
-				<Button className='BertButton-cancel'>Cancel</Button>
+				<Button className='BertButton-success'>Success</Button>
+				<Button className='BertButton-info'>Info</Button>
+				<Button className='BertButton-warning'>Warning</Button>
+				<Button className='BertButton-danger'>Danger</Button>
+				<Button className='BertButton-link'>link</Button>
 				<Button isActive={true}>Active</Button>
 				<Button isDisabled={true}>Disabled</Button>
 			</div>
