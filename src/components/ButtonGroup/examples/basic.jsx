26edf3ed8d8fe4222b5233a97625ee714f6a38df
Jon de la Motte <jdelamotte@appnexus.com>
2016-02-16 09:38:14 -0800

    Minor update to ButtonGroup example

--- src/components/ButtonGroup/examples/basic.jsx
+++ src/components/ButtonGroup/examples/basic.jsx
@@ -5,16 +5,14 @@ import ButtonGroup from '../ButtonGroup';
 export default React.createClass({
 	render() {
 		return (
-			<div>
-				<ButtonGroup>
-					<Button>One</Button>
-					<Button>Two</Button>
-					<Button>Three</Button>
-					<Button kind='primary'>Four</Button>
-					<Button isDisabled={true}>Five</Button>
-					<Button>Six</Button>
-				</ButtonGroup>
-			</div>
+			<ButtonGroup>
+				<Button>One</Button>
+				<Button>Two</Button>
+				<Button>Three</Button>
+				<Button isActive={true}>Four</Button>
+				<Button isDisabled={true}>Five</Button>
+				<Button>Six</Button>
+			</ButtonGroup>
 		);
 	}
 });
