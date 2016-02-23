4750fc26d44197360473da0e98c8b8ad518a7acd
Jon de la Motte <jdelamotte@appnexus.com>
2016-02-23 14:31:14 -0800

    Fix variable names and example

--- src/components/TextField/examples/basic.jsx
+++ src/components/TextField/examples/basic.jsx
@@ -5,7 +5,7 @@ export default React.createClass({
 	render() {
 		return (
 			<div>
-				<TextField style={{width: '300px'}} />
+				<TextField />
 				<TextField placeholder='Enter some text'/>
 				<TextField Error='No bueno' />
 				<TextField value='Cant type me' isDisabled={true} />
