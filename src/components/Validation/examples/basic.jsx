c4d747b03ff8d05497a717997419987f58f77ecb
Jon de la Motte <jdelamotte@appnexus.com>
2016-02-29 09:51:37 -0800

    Cleanup less variables and examples

--- src/components/Validation/examples/basic.jsx
+++ src/components/Validation/examples/basic.jsx
@@ -5,11 +5,11 @@ export default React.createClass({
 	render() {
 		return (
 			<div>
-				<Validation>Content</Validation>
 				<Validation Error='Method one'>Content</Validation>
-				<Validation Error={<i>Method two</i>}>Content</Validation>
 				<Validation>
-					<Validation.Error>Method three</Validation.Error>
+					<Validation.Error>
+						<i>Method two</i>
+					</Validation.Error>
 					Content
 				</Validation>
 			</div>
