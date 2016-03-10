f46c9cb6d5a8b1b3ae1ceac13963359c230a9c29
Dan Hahn <dhahn@appnexus.com>
2016-03-10 09:59:16 -0500

    Fix formatting error

--- src/components/Tabs/examples/stateful-progression.jsx
+++ src/components/Tabs/examples/stateful-progression.jsx
@@ -12,7 +12,7 @@ export default React.createClass({
 					<StatefulTabs.Tab Title='One'>One content</StatefulTabs.Tab>
 					<StatefulTabs.Tab Title='Two'>Two content</StatefulTabs.Tab>
 					<StatefulTabs.Tab Title='Three'>Three content</StatefulTabs.Tab>
-					<StatefulTabs.Tab Title='Disabled' isDisabled={true}>Disabled Content</StatefulTabs.Tab>					
+					<StatefulTabs.Tab Title='Disabled' isDisabled={true}>Disabled Content</StatefulTabs.Tab>
 					<StatefulTabs.Tab Title='Five'>Four content</StatefulTabs.Tab>
 				</StatefulTabs>
 			</div>
