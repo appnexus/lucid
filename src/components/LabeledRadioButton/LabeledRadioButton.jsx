b0cdb276871bff9bbd53dfe604f9f162ad29caa1
Nathaniel Lee <nlee@appnexus.com>
2016-03-11 15:01:59 -0500

    #86 -- Removed explicit declarations of prop types duplicated from RadioButton in favor of just spreading in RadioButton.propTypes. Added to the custom JSON for doc generation so the example page shows that LabeledRadioButton is composed from RadioButton.

--- src/components/LabeledRadioButton/LabeledRadioButton.jsx
+++ src/components/LabeledRadioButton/LabeledRadioButton.jsx
@@ -14,50 +14,20 @@ const {
 } = React.PropTypes;
 
 /**
- * {"categories": ["controls", "toggles"]}
+ * {"categories": ["controls", "toggles"], "extend": "RadioButton"}
  *
  * This is a composite of the `RadioButton` component and the native `label`
  * element.
  */
 const LabeledRadioButton = React.createClass({
 	propTypes: {
-		/**
-		 * Appended to the component-specific class names set on the root
-		 * element.
-		 */
-		className: string,
-
-		/**
-		 * Indicates whether the component should appear and act disabled by
-		 * having a "greyed out" palette and ignoring user interactions.
-		 */
-		isDisabled: bool,
-
-		/**
-		 * Indicates that the component is in the "selected" state when true
-		 * and in the "unselected" state when false.
-		 */
-		isSelected: bool,
+		...RadioButton.propTypes,
 
 		/**
 		 * Used to identify the purpose of this radio button to the user -- can
 		 * be any renderable content.
 		 */
-		label: node,
-
-		/**
-		 * Called when the user clicks on the component or when they press the
-		 * space key while the component is in focus, and only called when the
-		 * component is in the unselected state.
-		 *
-		 * Signature: `(true, { event, props }) => {}`
-		 */
-		onSelect: func,
-
-		/**
-		 * Passed through to the root element.
-		 */
-		style: object
+		label: node
 	},
 
 	getDefaultProps() {
