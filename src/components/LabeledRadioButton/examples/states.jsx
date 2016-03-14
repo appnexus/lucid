fca23f002a8493d08a8918a158dfee66e45b4d4f
Nathaniel Lee <nlee@appnexus.com>
2016-03-14 12:07:53 -0400

    #86 -- Removed extraneous white space to address linting concern.

--- src/components/LabeledRadioButton/examples/states.jsx
+++ src/components/LabeledRadioButton/examples/states.jsx
@@ -14,7 +14,7 @@ export default React.createClass({
 				<LabeledRadioButton Label='String passed in as the label' style={style} />
 				<LabeledRadioButton Label={<p>HTML element passed in as the label</p>} style={style} />
 				<LabeledRadioButton Label={[
-					'First in an array passed in as the label', 
+					'First in an array passed in as the label',
 					'Second in an array passed in as the label',
 					'Third in an array passed in as the label'
 				]} style={style} />
