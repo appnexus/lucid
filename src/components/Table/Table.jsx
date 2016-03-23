cce5dfe1570dd8c8ff550539d8c84cb0217bf1c7
Dan Hahn <dhahn@appnexus.com>
2016-03-23 16:32:00 -0400

    Add new styles for * colspan * rowspan * border left/right * sort colum

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -62,13 +62,18 @@ const Table = React.createClass({
 						'&-isComfortable': isComfortable,
 					})}>
 					<thead className={boundClassNames('&-thead')}>
-						<tr>
-							<th className={boundClassNames('&-checkbox', '&-cell')}>
+						<tr className={boundClassNames('&-thead-row')}>
+							<th className={boundClassNames('&-cell')} rowSpan="2">RS</th>
+							<th className={boundClassNames('&-cell', '&-checkbox')} rowSpan="2">
 								<Checkbox></Checkbox>
 							</th>
-							<th className={boundClassNames('&-cell')}>Lorem.</th>
-							<th className={boundClassNames('&-cell', '&-hasIcon')}><SuccessIcon/></th>
-							<th className={boundClassNames('&-cell')}>Button</th>
+							<th className={boundClassNames('&-cell')} rowSpan="2">Lorem.</th>
+							<th className={boundClassNames('&-cell', '&-hasIcon')} rowSpan="2"><SuccessIcon/></th>
+							<th className={boundClassNames('&-cell')} rowSpan="2">Button</th>
+							<th className={boundClassNames('&-cell', '&-isSorted')} rowSpan="2">Sorted Column</th>
+							<th className={boundClassNames('&-cell', '&-align-center')} colSpan="3">Alignments</th>
+						</tr>
+						<tr className={boundClassNames('&-thead-row')}>
 							<th className={boundClassNames('&-cell', '&-align-left')}>align left</th>
 							<th className={boundClassNames('&-cell', '&-align-center')}>align center</th>
 							<th className={boundClassNames('&-cell', '&-align-right')}>align right</th>
@@ -76,65 +81,93 @@ const Table = React.createClass({
 					</thead>
 					<tbody className={boundClassNames('&-tbody')}>
 						<tr className={boundClassNames('&-row')}>
-							<td className={boundClassNames('&-cell', '&-checkbox')}><Checkbox/></td>
-							<td className={boundClassNames('&-cell')}><a href="#">Link</a></td>
-							<td className={boundClassNames('&-cell', '&-hasIcon')}><SuccessIcon/></td>
+							<td className={boundClassNames('&-cell', '&-hasBorderRight', '&-hasBorderLeft', '&-hasRowSpan')} rowSpan="9">RS</td>
+							<td className={boundClassNames('&-cell', '&-checkbox', '&-fixHover')}><Checkbox/></td>
+							<td className={boundClassNames('&-cell')}>Text</td>
+							<td className={boundClassNames('&-cell', '&-hasIcon', '&-hasBorderRight', '&-hasBorderLeft')}><SuccessIcon/></td>
 							<td className={boundClassNames('&-cell', '&-hasButton')}><Button size="small">button</Button></td>
-							<td className={boundClassNames('&-cell', '&-align-left')}>align left</td>
+							<td className={boundClassNames('&-cell')}>Sorted Column</td>
+							<td className={boundClassNames('&-cell', '&-align-left', '&-hasBorderLeft')}>align left</td>
 							<td className={boundClassNames('&-cell', '&-align-center')}>align center</td>
 							<td className={boundClassNames('&-cell', '&-align-right')}>align right</td>
 						</tr>
 						<tr className={boundClassNames('&-row', '&-isDisabled')}>
 							<td className={boundClassNames('&-cell', '&-checkbox')}><Checkbox/></td>
-							<td className={boundClassNames('&-cell')}><a href="#">isDisabled</a></td>
-							<td className={boundClassNames('&-cell', '&-hasIcon')}><SuccessIcon/></td>
+							<td className={boundClassNames('&-cell')}>isDisabled</td>
+							<td className={boundClassNames('&-cell', '&-hasIcon', '&-hasBorderRight', '&-hasBorderLeft')}><SuccessIcon/></td>
 							<td className={boundClassNames('&-cell', '&-hasButton')}><Button size="small" isDisabled={true}>button</Button></td>
-							<td className={boundClassNames('&-cell', '&-align-left')}>align left</td>
+							<td className={boundClassNames('&-cell')}>Sorted Column</td>
+							<td className={boundClassNames('&-cell', '&-align-left', '&-hasBorderLeft')}>align left</td>
+							<td className={boundClassNames('&-cell', '&-align-center')}>align center</td>
+							<td className={boundClassNames('&-cell', '&-align-right')}>align right</td>
+						</tr>
+						<tr className={boundClassNames('&-row')}>
+							<td className={boundClassNames('&-cell', '&-checkbox')}><Checkbox/></td>
+							<td className={boundClassNames('&-cell')}><a href="#">Link</a></td>
+							<td className={boundClassNames('&-cell', '&-hasIcon', '&-hasBorderRight', '&-hasBorderLeft')}><SuccessIcon/></td>
+							<td className={boundClassNames('&-cell', '&-hasButton')}><Button size="small">button</Button></td>
+							<td className={boundClassNames('&-cell')}>Sorted Column</td>
+							<td className={boundClassNames('&-cell', '&-align-left', '&-hasBorderLeft')}>align left</td>
+							<td className={boundClassNames('&-cell', '&-align-center')}>align center</td>
+							<td className={boundClassNames('&-cell', '&-align-right')}>align right</td>
+						</tr>
+						<tr className={boundClassNames('&-row', '&-isDisabled')}>
+							<td className={boundClassNames('&-cell', '&-checkbox')}><Checkbox/></td>
+							<td className={boundClassNames('&-cell')}><a href="#">isDisabled Link</a></td>
+							<td className={boundClassNames('&-cell', '&-hasIcon', '&-hasBorderRight', '&-hasBorderLeft')}><SuccessIcon/></td>
+							<td className={boundClassNames('&-cell', '&-hasButton')}><Button size="small" isDisabled={true}>button</Button></td>
+							<td className={boundClassNames('&-cell')}>Sorted Column</td>
+							<td className={boundClassNames('&-cell', '&-align-left', '&-hasBorderLeft')}>align left</td>
 							<td className={boundClassNames('&-cell', '&-align-center')}>align center</td>
 							<td className={boundClassNames('&-cell', '&-align-right')}>align right</td>
 						</tr>
 						<tr className={boundClassNames('&-row', '&-isChecked')}>
 							<td className={boundClassNames('&-cell', '&-checkbox')}><Checkbox isSelected={true}/></td>
-							<td className={boundClassNames('&-cell')}><a href="#">isChecked</a></td>
-							<td className={boundClassNames('&-cell', '&-hasIcon')}><SuccessIcon/></td>
+							<td className={boundClassNames('&-cell')}>isChecked</td>
+							<td className={boundClassNames('&-cell', '&-hasIcon', '&-hasBorderRight', '&-hasBorderLeft')}><SuccessIcon/></td>
 							<td className={boundClassNames('&-cell', '&-hasButton')}><Button size="small">button</Button></td>
-							<td className={boundClassNames('&-cell', '&-align-left')}>align left</td>
+							<td className={boundClassNames('&-cell')}>Sorted Column</td>
+							<td className={boundClassNames('&-cell', '&-align-left', '&-hasBorderLeft')}>align left</td>
 							<td className={boundClassNames('&-cell', '&-align-center')}>align center</td>
 							<td className={boundClassNames('&-cell', '&-align-right')}>align right</td>
 						</tr>
 						<tr className={boundClassNames('&-row', '&-isChecked', '&-isDisabled')}>
 							<td className={boundClassNames('&-cell', '&-checkbox')}><Checkbox isSelected={true}/></td>
-							<td className={boundClassNames('&-cell')}><a href="#">isChecked && isDisabled</a></td>
-							<td className={boundClassNames('&-cell', '&-hasIcon')}><SuccessIcon/></td>
+							<td className={boundClassNames('&-cell')}>isChecked && isDisabled</td>
+							<td className={boundClassNames('&-cell', '&-hasIcon', '&-hasBorderRight', '&-hasBorderLeft')}><SuccessIcon/></td>
 							<td className={boundClassNames('&-cell', '&-hasButton')}><Button size="small" isDisabled={true}>button</Button></td>
-							<td className={boundClassNames('&-cell', '&-align-left')}>align left</td>
+							<td className={boundClassNames('&-cell')}>Sorted Column</td>
+							<td className={boundClassNames('&-cell', '&-align-left', '&-hasBorderLeft')}>align left</td>
 							<td className={boundClassNames('&-cell', '&-align-center')}>align center</td>
 							<td className={boundClassNames('&-cell', '&-align-right')}>align right</td>
 						</tr>
 						<tr className={boundClassNames('&-row', '&-isSelected')}>
 							<td className={boundClassNames('&-cell', '&-checkbox')}><Checkbox/></td>
-							<td className={boundClassNames('&-cell')}><a href="#">isSelected</a></td>
-							<td className={boundClassNames('&-cell', '&-hasIcon')}><SuccessIcon/></td>
+							<td className={boundClassNames('&-cell')}>isSelected</td>
+							<td className={boundClassNames('&-cell', '&-hasIcon', '&-hasBorderRight', '&-hasBorderLeft')}><SuccessIcon/></td>
 							<td className={boundClassNames('&-cell', '&-hasButton')}><Button size="small">button</Button></td>
-							<td className={boundClassNames('&-cell', '&-align-left')}>align left</td>
+							<td className={boundClassNames('&-cell')}>Sorted Column</td>
+							<td className={boundClassNames('&-cell', '&-align-left', '&-hasBorderLeft')}>align left</td>
 							<td className={boundClassNames('&-cell', '&-align-center')}>align center</td>
 							<td className={boundClassNames('&-cell', '&-align-right')}>align right</td>
 						</tr>
 						<tr className={boundClassNames('&-row', '&-isSelected', '&-isDisabled')}>
 							<td className={boundClassNames('&-cell', '&-checkbox')}><Checkbox/></td>
-							<td className={boundClassNames('&-cell')}><a href="#">isSelected && isDisabled</a></td>
-							<td className={boundClassNames('&-cell', '&-hasIcon')}><SuccessIcon/></td>
+							<td className={boundClassNames('&-cell')}>isSelected && isDisabled</td>
+							<td className={boundClassNames('&-cell', '&-hasIcon', '&-hasBorderRight', '&-hasBorderLeft')}><SuccessIcon/></td>
 							<td className={boundClassNames('&-cell', '&-hasButton')}><Button size="small" isDisabled={true}>button</Button></td>
-							<td className={boundClassNames('&-cell', '&-align-left')}>align left</td>
+							<td className={boundClassNames('&-cell')}>Sorted Column</td>
+							<td className={boundClassNames('&-cell', '&-align-left', '&-hasBorderLeft')}>align left</td>
 							<td className={boundClassNames('&-cell', '&-align-center')}>align center</td>
 							<td className={boundClassNames('&-cell', '&-align-right')}>align right</td>
 						</tr>
 						<tr className={boundClassNames('&-row', '&-isHover')}>
 							<td className={boundClassNames('&-cell', '&-checkbox')}><Checkbox/></td>
-							<td className={boundClassNames('&-cell')}><a href="#">:hover</a></td>
-							<td className={boundClassNames('&-cell', '&-hasIcon')}><SuccessIcon/></td>
+							<td className={boundClassNames('&-cell')}>:hover</td>
+							<td className={boundClassNames('&-cell', '&-hasIcon', '&-hasBorderRight', '&-hasBorderLeft')}><SuccessIcon/></td>
 							<td className={boundClassNames('&-cell', '&-hasButton')}><Button size="small">button</Button></td>
-							<td className={boundClassNames('&-cell', '&-align-left')}>align left</td>
+							<td className={boundClassNames('&-cell')}>Sorted Column</td>
+							<td className={boundClassNames('&-cell', '&-align-left', '&-hasBorderLeft')}>align left</td>
 							<td className={boundClassNames('&-cell', '&-align-center')}>align center</td>
 							<td className={boundClassNames('&-cell', '&-align-right')}>align right</td>
 						</tr>
