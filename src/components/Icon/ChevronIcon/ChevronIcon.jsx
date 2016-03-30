2b825ab48e8fed517ef5caa36b942819b652f14d
Nathaniel Lee <nlee@appnexus.com>
2016-03-30 15:38:07 -0700

    #152 -- Added ChevronIcon component.

---
+++ src/components/Icon/ChevronIcon/ChevronIcon.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { lucidClassNames } from '../../../util/style-helpers';
+import Icon from '../Icon';
+
+const boundClassNames = lucidClassNames.bind('&-ChevronIcon');
+
+const {
+	oneOf,
+} = React.PropTypes;
+
+/**
+ *
+ * {"categories": ["visual design", "icons"], "extend": "Icon", "madeFrom": ["Icon"]}
+ *
+ * A chevron icon.
+ */
+const ChevronIcon = React.createClass({
+	propTypes: {
+		...Icon.propTypes,
+		/**
+		 * direction variations of the icon
+		 */
+		direction: oneOf([
+			'up',
+			'down',
+			'left',
+			'right',
+		]),
+	},
+
+	getDefaultProps() {
+		return {
+			direction: 'down',
+			// viewBox: '0 3 16 8',
+			viewBox: '0 0 16 16',
+			size: 16
+		};
+	},
+
+	render() {
+		const {
+			className,
+			direction,
+			size,
+			...passThroughs
+		} = this.props;
+
+		return (
+			<Icon
+				{...passThroughs}
+				className={boundClassNames('&', {
+					'&-is-down': direction === 'down',
+					'&-is-up': direction === 'up',
+					'&-is-left': direction === 'left',
+					'&-is-right': direction === 'right',
+				}, className)}
+				size={size}
+			>
+				<path d='M3,6 L8,11 L13,6' strokeWidth='2' strokeLinejoin='round' />
+			</Icon>
+		);
+	}
+});
+
+export default ChevronIcon;
