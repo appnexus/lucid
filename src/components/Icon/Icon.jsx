d72e4e116f183e2846f2e966518de5c5d80b12dc
Jon de la Motte <jdelamotte@appnexus.com>
2016-03-30 11:18:19 -0700

    Fixes #143 and fixes #50 by tweaking icons
    - Normalized all icons and set the default back to 16x16
    - EligibilityIcon had left and right backward and I simplified the less
    - Improved docs to handle `extend` better for default values

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React from 'react';
 import { lucidClassNames } from '../../util/style-helpers';
 
@@ -7,7 +8,8 @@ const {
 	any,
 	string,
 	number,
-	bool
+	object,
+	bool,
 } = React.PropTypes;
 
 /**
@@ -19,6 +21,10 @@ const {
  */
 const Icon = React.createClass({
 	propTypes: {
+		/**
+		 * Styles that are passed through to the `svg`.
+		 */
+		style: object,
 		/**
 		 * Classes that are appended to the component defaults. This prop is run
 		 * through the `classnames` library.
@@ -51,9 +57,9 @@ const Icon = React.createClass({
 
 	getDefaultProps() {
 		return {
-			size: 18,
+			size: 16,
 			aspectRatio: 'xMidYMid meet',
-			viewBox: '0 0 18 18',
+			viewBox: '0 0 16 16',
 			isBadge: false
 		};
 	},
@@ -63,15 +69,25 @@ const Icon = React.createClass({
 			className,
 			children,
 			size,
+			style,
 			viewBox,
 			aspectRatio,
 			isBadge,
 			...passThroughs
 		} = this.props;
 
+		// Because we control the icon size inline, we must also control the border
+		// radius in the case where they user wants `isBadge`
+		const borderRadius = _.get(style, 'borderRadius', isBadge ? `${size}px` : null);
+		const actualStyle = {
+			...style,
+			borderRadius
+		};
+
 		return (
 			<svg
 				{...passThroughs}
+				style={actualStyle}
 				className={boundClassNames('&', {
 					'&-is-badge': isBadge,
 				}, className)}
