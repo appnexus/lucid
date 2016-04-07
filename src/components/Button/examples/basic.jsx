489d714d541e78ee2e054e1e91cedea39955f68e
Jon de la Motte <jdelamotte@appnexus.com>
2016-04-07 11:55:54 -0700

    Revert "#135, #164, #170: bugfix button styling"
    This reverts commit bb4a1e133372161aeedf64ea27ff3d2899373603.
    
    It was missing UX approval and it had a regression in the active
    styling.

--- src/components/Button/examples/basic.jsx
+++ src/components/Button/examples/basic.jsx
@@ -1,34 +1,76 @@
 import React from 'react';
 import Button from '../Button';
-import CheckIcon from '../../Icon/CheckIcon/CheckIcon';
 
 export default React.createClass({
 	render() {
 		return (
-			<section>
-				<article>
+			<div>
+				<div>
 					<Button>Default</Button>
-					<Button><CheckIcon /> has icon</Button>
-					<Button isDisabled={true}>is disabled</Button>
-					<Button isActive={true}>is active</Button>
-				</article>
-				<article>
-					<Button size='short'>short</Button>
-					<Button size='short' isDisabled={true}>short is disabled</Button>
-					<Button size='short' isActive={true}>short is active</Button>
-				</article>
-				<article>
-					<Button size='small'>small</Button>
-					<Button size='small' isDisabled={true}>small is disabled</Button>
-					<Button size='small' isActive={true}>small is active</Button>
-				</article>
-				<article>
-					<Button size='large'>large</Button>
-					<Button size='large'><CheckIcon /> large has icon</Button>
-					<Button size='large' isDisabled={true}>large is disabled</Button>
-					<Button size='large' isActive={true}>large is active</Button>
-				</article>
-			</section>
+					<Button isDisabled={true}>Default</Button>
+					<Button isActive={true}>Default</Button>
+					<Button size='short'>Default</Button>
+					<Button size='short' isDisabled={true}>Default</Button>
+					<Button size='small'>Default</Button>
+					<Button size='small' isDisabled={true}>Default</Button>
+					<Button size='large'>Default</Button>
+					<Button size='large' isDisabled={true}>Default</Button>
+				</div>
+				<div>
+					<Button kind='primary'>Primary</Button>
+					<Button kind='primary' isDisabled={true}>Primary</Button>
+					<Button kind='primary' size='short'>Primary</Button>
+					<Button kind='primary' size='short' isDisabled={true}>Primary</Button>
+					<Button kind='primary' size='small'>Primary</Button>
+					<Button kind='primary' size='small' isDisabled={true}>Primary</Button>
+					<Button kind='primary' size='large'>Primary</Button>
+					<Button kind='primary' size='large' isDisabled={true}>Primary</Button>
+				</div>
+				<div>
+					<Button kind='success'>Success</Button>
+					<Button kind='success' isDisabled={true}>Success</Button>
+					<Button kind='success' size='short'>Success</Button>
+					<Button kind='success' size='short' isDisabled={true}>Success</Button>
+					<Button kind='success' size='small'>Success</Button>
+					<Button kind='success' size='small' isDisabled={true}>Success</Button>
+					<Button kind='success' size='large'>Success</Button>
+					<Button kind='success' size='large' isDisabled={true}>Success</Button>
+				</div>
+				<div>
+					<Button kind='warning'>Warning</Button>
+					<Button kind='warning' isDisabled={true}>Warning</Button>
+					<Button kind='warning' size='short'>Warning</Button>
+					<Button kind='warning' size='short' isDisabled={true}>Warning</Button>
+					<Button kind='warning' size='small'>Warning</Button>
+					<Button kind='warning' size='small' isDisabled={true}>Warning</Button>
+					<Button kind='warning' size='large'>Warning</Button>
+					<Button kind='warning' size='large' isDisabled={true}>Warning</Button>
+				</div>
+				<div>
+					<Button kind='danger'>Danger</Button>
+					<Button kind='danger' isDisabled={true}>Danger</Button>
+					<Button kind='danger' size='short'>Danger</Button>
+					<Button kind='danger' size='short' isDisabled={true}>Danger</Button>
+					<Button kind='danger' size='small'>Danger</Button>
+					<Button kind='danger' size='small' isDisabled={true}>Danger</Button>
+					<Button kind='danger' size='large'>Danger</Button>
+					<Button kind='danger' size='large' isDisabled={true}>Danger</Button>
+				</div>
+				<div>
+					<Button kind='info'>Info</Button>
+					<Button kind='info' isDisabled={true}>Info</Button>
+					<Button kind='info' size='short'>Info</Button>
+					<Button kind='info' size='short' isDisabled={true}>Info</Button>
+					<Button kind='info' size='small'>Info</Button>
+					<Button kind='info' size='small' isDisabled={true}>Info</Button>
+					<Button kind='info' size='large'>Info</Button>
+					<Button kind='info' size='large' isDisabled={true}>Info</Button>
+				</div>
+				<div>
+					<Button kind='link'>Link</Button>
+					<Button kind='link' isDisabled={true}>Link</Button>
+				</div>
+			</div>
 		);
 	}
 });
