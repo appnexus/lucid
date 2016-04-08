399efbaae5d329c982cd127a95bb177be7446432
Jon de la Motte <jdelamotte@appnexus.com>
2016-04-08 11:57:05 -0700

    Clean up examples and fix small flex bug

--- src/components/List/examples/3.nested-full-width.jsx
+++ src/components/List/examples/3.nested-full-width.jsx
@@ -23,27 +23,36 @@ export default React.createClass({
 	render() {
 		return (
 			<List
-				style={{ width: '200px' }}
 				onSelect={_.partial(this.handleSelect, 'one')}
 				selectedIndices={this.state.currentList === 'one' ? this.state.selectedIndices : []}
 			>
 				<List.Item>Level one</List.Item>
-				<List.Item hasExpander={true}>
+				<List.Item isExpanded={true}>
 					Level one with List
 					<List
 						onSelect={_.partial(this.handleSelect, 'two')}
 						selectedIndices={this.state.currentList === 'two' ? this.state.selectedIndices : []}
 					>
 						<List.Item>Level two</List.Item>
-						<List.Item hasExpander={true}>
-							Level two with List
+						<List.Item>Level two</List.Item>
+					</List>
+				</List.Item>
+				<List.Item isExpanded={true}>
+					Level one with List
+					<List
+						onSelect={_.partial(this.handleSelect, 'three')}
+						selectedIndices={this.state.currentList === 'three' ? this.state.selectedIndices : []}
+					>
+						<List.Item>Level two</List.Item>
+						<List.Item isExpanded={false}>
+							Level two with closed List
 							<List
-								onSelect={_.partial(this.handleSelect, 'three')}
-								selectedIndices={this.state.currentList === 'three' ? this.state.selectedIndices : []}
+								onSelect={_.partial(this.handleSelect, 'four')}
+								selectedIndices={this.state.currentList === 'four' ? this.state.selectedIndices : []}
 							>
-								{_.times(50, (n) => {
-									return <List.Item key={n}>Level three</List.Item>;
-								})}
+								<List.Item>Level three</List.Item>
+								<List.Item>Level three</List.Item>
+								<List.Item>Level three</List.Item>
 							</List>
 						</List.Item>
 						<List.Item>Level two</List.Item>
