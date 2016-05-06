bde4b1ceaed28e5907aa7aea7e6e450a2bf1f55f
Oliver Gupte <ogupte@appnexus.com>
2016-05-06 13:37:38 -0700

    reapply changes from b65ee7dec9d420623e8c875ca53e21c105bec31b that were lost in merge conflicts

--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ import { default as DropMenuDumb } from './components/DropMenu/DropMenu';
 import { default as ExpanderDumb } from './components/Expander/Expander';
 import { default as SingleSelectDumb } from './components/SingleSelect/SingleSelect';
 import { default as TabsDumb } from './components/Tabs/Tabs';
-import { default as TextFieldDumb } from './components/TextField/TextField'
-import { default as TextFieldValidatedDumb } from './components/TextFieldValidated/TextFieldValidated';
 import { default as VerticalListMenuDumb } from './components/VerticalListMenu/VerticalListMenu';
 import { default as RadioGroupDumb } from './components/RadioGroup/RadioGroup';
 
@@ -19,8 +17,6 @@ const Expander = buildHybridComponent(ExpanderDumb);
 const RadioGroup = buildHybridComponent(RadioGroupDumb);
 const SingleSelect = buildHybridComponent(SingleSelectDumb);
 const Tabs = buildHybridComponent(TabsDumb);
-const TextField = buildHybridComponent(TextFieldDumb);
-const TextFieldValidated = buildHybridComponent(TextFieldValidatedDumb);
 const VerticalListMenu = buildHybridComponent(VerticalListMenuDumb);
 
 // dumb components
@@ -58,6 +54,8 @@ import StickySection from './components/StickySection/StickySection';
 import SuccessIcon from './components/Icon/SuccessIcon/SuccessIcon';
 import Switch from './components/Switch/Switch';
 import Table from './components/Table/Table';
+import TextField from './components/TextField/TextField';
+import TextFieldValidated from './components/TextFieldValidated/TextFieldValidated';
 import Validation from './components/Validation/Validation';
 import WarningIcon from './components/Icon/WarningIcon/WarningIcon';
 
@@ -126,9 +124,7 @@ export {
 	Tabs,
 	TabsDumb,
 	TextField,
-	TextFieldDumb,
 	TextFieldValidated,
-	TextFieldValidatedDumb,
 	Validation,
 	VerticalListMenu,
 	VerticalListMenuDumb,
