0325c711dd642ba1d72c5dec94c8469bb591c1b4
Joe Moon <joe@xoxomoon.com>
2016-05-16 11:41:20 -0700

    uncomment static directions example

--- src/components/ContextMenu/examples/5.directions-static.jsx
+++ src/components/ContextMenu/examples/5.directions-static.jsx
@@ -1,58 +1,69 @@
 import React from 'react';
 import { ContextMenu, Button } from '../../../index';
 
+const {
+	CENTER,
+	DOWN,
+	END,
+	LEFT,
+	RIGHT,
+	START,
+	UP,
+} = ContextMenu;
+
+const directions = [UP, DOWN, LEFT, RIGHT];
+const alignments = [START, CENTER, END];
+
 export default React.createClass({
 
 	render() {
 
-		return null;
+		const style = {
+			background: 'white',
+			boxShadow: '1px 1px 4px black',
+			padding: 4
+		};
 
-		// const style = {
-		// 	background: 'white',
-		// 	boxShadow: '1px 1px 4px black',
-		// 	padding: 4
-		// };
+		return (
+			<section
+				style={{
+					display: 'flex',
+					flexDirection: 'row',
+					margin: '0 60px'
+				}}
+			>
 
-		// return (
-		// 	<section
-		// 		style={{
-		// 			display: 'flex',
-		// 			flexDirection: 'row',
-		// 			margin: '0 60px'
-		// 		}}
-		// 	>
+				{_.map(directions, direction => {
+					return (
+						<section key={direction} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', flexGrow: 1 }}>
+							{_.map(alignments, alignment => _.map([0, 15, -15], directonOffset => _.map([0, 15, -15], alignmentOffset => (
+								<div key={`${alignment}${alignmentOffset}${directonOffset}`} style={{marginTop: '120px'}}>
+									<ContextMenu
+										{...{
+											direction,
+											directonOffset,
+											alignment,
+											alignmentOffset,
+										}}
+										>
+											<ContextMenu.Target>
+												Target
+											</ContextMenu.Target>
 
-		// 		{_.map(['left', 'up', 'down', 'right'], direction => {
-		// 			return (
-		// 				<section key={direction} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', flexGrow: 1 }}>
-		// 					{_.map(['start', 'center', 'end'], alignment => _.map([0, 15, -15], directonOffset => _.map([0, 15, -15], alignmentOffset => (
-		// 						<div key={`${alignment}${alignmentOffset}${directonOffset}`} style={{marginTop: '120px'}}>
-		// 							<ContextMenu
-		// 								{...{
-		// 									direction,
-		// 									directonOffset,
-		// 									alignment,
-		// 									alignmentOffset,
-		// 								}}
-		// 								>
-		// 									<ContextMenu.Target>
-		// 										Target
-		// 									</ContextMenu.Target>
+											<ContextMenu.FlyOut style={style}>
+												<div>{`direction: ${direction}`}</div>
+												<div>{`directonOffset: ${directonOffset}`}</div>
+												<div>{`alignment: ${alignment}`}</div>
+												<div>{`alignmentOffset: ${alignmentOffset}`}</div>
+											</ContextMenu.FlyOut>
+										</ContextMenu>
+									</div>
+							 ))))}
+						 </section>
+					 );
+			 })}
 
-		// 									<ContextMenu.FlyOut style={style}>
-		// 										<div>{`direction: ${direction}`}</div>
-		// 										<div>{`directonOffset: ${directonOffset}`}</div>
-		// 										<div>{`alignment: ${alignment}`}</div>
-		// 										<div>{`alignmentOffset: ${alignmentOffset}`}</div>
-		// 									</ContextMenu.FlyOut>
-		// 								</ContextMenu>
-		// 							</div>
-		// 					 ))))}
-		// 				 </section>
-		// 			 );
-		// 	 })}
-
-		// 	</section>
-		// );
+			</section>
+		);
 	}
 });
