0f7f420129072e6d5e11805a9e8ab72bbd86986f
Esha Peddireddy <epeddireddy@appnexus.com>
2016-06-15 11:42:26 -0700

    #347: Pass through event to Paginator onPageSelect

--- src/components/Paginator/Paginator.jsx
+++ src/components/Paginator/Paginator.jsx
@@ -86,7 +86,7 @@ const Paginator = createClass({
 		TextField: shape(TextField.propTypes),
 		/**
 		 * Called when a page is selected.
-		 * Has the signature `(pageIndex, {props, event}) => {}` where pageIndex is a number.
+		 * Has the signature `(pageIndex, totalPages, {props, event}) => {}` where pageIndex is a number.
 		 */
 		onPageSelect: func,
 		/**
@@ -113,7 +113,7 @@ const Paginator = createClass({
 		};
 	},
 
-	handleTextFieldChange(pageNum) {
+	handleTextFieldChange(pageNum, {props, event}) {
 		const {
 			onPageSelect,
 			selectedPageIndex,
@@ -121,9 +121,9 @@ const Paginator = createClass({
 		} = this.props;
 		const parsedPageNum = _.parseInt(pageNum);
 		if (_.isNaN(parsedPageNum)) {
-			return onPageSelect(selectedPageIndex, totalPages);
+			return onPageSelect(selectedPageIndex, totalPages, {props, event});
 		}
-		return onPageSelect(parsedPageNum - 1, totalPages);
+		return onPageSelect(parsedPageNum - 1, totalPages, {props, event});
 	},
 
 	render() {
