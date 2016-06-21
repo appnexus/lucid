612bb9fef450f50e2bde50943a62e9f86cc66933
Joe Moon <jmoon@appnexus.com>
2016-06-21 10:33:06 -0700

    remove isCloseable and kind props

--- src/components/LoadingIndicator/LoadingIndicator.jsx
+++ src/components/LoadingIndicator/LoadingIndicator.jsx
@@ -4,8 +4,6 @@ import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import { lucidClassNames } from '../../util/style-helpers';
 import { createClass, findTypes, rejectTypes } from '../../util/component-types';
 import LoadingIcon from '../Icon/LoadingIcon/LoadingIcon';
-import SuccessIcon from '../Icon/SuccessIcon/SuccessIcon';
-import CrossIcon from '../Icon/CrossIcon/CrossIcon';
 
 const cx = lucidClassNames.bind('&-LoadingIndicator');
 
@@ -14,12 +12,10 @@ const {
 	element,
 	func,
 	node,
-	oneOf,
 	string,
 } = React.PropTypes;
 
 const defaultIcons = {
-	success: <SuccessIcon />,
 	default: <LoadingIcon />,
 };
 
@@ -43,10 +39,6 @@ const LoadingIndicator = createClass({
 		 */
 		icon: element,
 		/**
-		 * Set this to `true` if you want to have an `x` close icon.
-		 */
-		isCloseable: bool,
-		/**
 		 * Class names that are appended to the defaults.
 		 */
 		className: string,
@@ -54,13 +46,6 @@ const LoadingIndicator = createClass({
 		 * Any valid React children.
 		 */
 		children: node,
-		/**
-		 * Style variations of the `LoadingIndicator`.
-		 */
-		kind: oneOf([
-			'success',
-			'default',
-		]),
 		/**
 		 * Called when the user closes the `LoadingIndicator`.
 		 *
@@ -88,42 +73,36 @@ const LoadingIndicator = createClass({
 	getDefaultProps() {
 		return {
 			hasOverlay: true,
-			isCloseable: false,
-			kind: 'default',
 			onClose: _.noop,
 			isLoading: false,
 		};
-	},
-
-	handleClose(event) {
-		this.props.onClose({ event, props: this.props });
 	},
 
 	render() {
 		const {
 			props,
 			props: {
-				kind,
 				hasOverlay,
-				icon = defaultIcons[kind],
+				icon = defaultIcons['default'],
 				className,
 				children,
-				isCloseable,
 				isLoading,
 				...passThroughs,
 			},
-			handleClose,
 		} = this;
 
-		const defaultTitle = icon === defaultIcons[kind] ? 'Loading' : null;
+		const defaultTitle = 'Loading';
 		const titleElement = _.first(findTypes(props, LoadingIndicator.Title));
-		const titleChildren = _.get(titleElement, 'props.children', defaultTitle);
+		const titleChildren = _.get(titleElement, 'props.children');
 		const bodyElement = _.first(findTypes(props, LoadingIndicator.Body));
 		const bodyChildren = _.get(bodyElement, 'props.children', null);
 		const otherChildren = rejectTypes(children, [LoadingIndicator.Title, LoadingIndicator.Body]);
 
 		return (
-			<div {...passThroughs} className={cx('&', '&-container', className)} >
+			<div
+				{..._.omit(passThroughs, ['Title', 'Body'])}
+				className={cx('&', '&-container', className)}
+			>
 				{otherChildren}
 				<ReactCSSTransitionGroup
 					transitionName={cx('&-banner-container')}
@@ -134,8 +113,7 @@ const LoadingIndicator = createClass({
 						<div className={cx('&-banner-container', { '&-has-overlay': hasOverlay })}>
 							<div className={cx('&-banner')}>
 								{icon}
-								{titleChildren && <h3 className={cx('&-title')}>{titleChildren}</h3>}
-								{isCloseable && <CrossIcon className={cx('&-close')} onClick={handleClose}/>}
+								{!_.isNull(titleChildren) && <h3 className={cx('&-title')}>{titleChildren || defaultTitle}</h3>}
 								{bodyChildren && <span className={cx('&-body')}>{bodyChildren}</span>}
 							</div>
 						</div>
