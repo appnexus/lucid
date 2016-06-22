55a64df8209bba011e30e5dc3146098987053de0
Oliver Gupte <ogupte@appnexus.com>
2016-06-22 14:58:12 -0700

    Add Sidebar component, tests, examples, and docs.
    fixes: #235

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { default as ExpanderDumb } from './components/Expander/Expander';
 import { default as ExpanderPanelDumb } from './components/ExpanderPanel/ExpanderPanel';
 import { default as PaginatorDumb } from './components/Paginator/Paginator';
 import { default as RadioGroupDumb } from './components/RadioGroup/RadioGroup';
+import { default as SidebarDumb } from './components/Sidebar/Sidebar';
 import { default as SingleSelectDumb } from './components/SingleSelect/SingleSelect';
 import { default as TabsDumb } from './components/Tabs/Tabs';
 import { default as ToolTipDumb } from './components/ToolTip/ToolTip';
@@ -20,6 +21,7 @@ const Expander = buildHybridComponent(ExpanderDumb);
 const ExpanderPanel = buildHybridComponent(ExpanderPanelDumb);
 const Paginator = buildHybridComponent(PaginatorDumb);
 const RadioGroup = buildHybridComponent(RadioGroupDumb);
+const Sidebar = buildHybridComponent(SidebarDumb);
 const SingleSelect = buildHybridComponent(SingleSelectDumb);
 const Tabs = buildHybridComponent(TabsDumb);
 const ToolTip = buildHybridComponent(ToolTipDumb);
@@ -138,6 +140,8 @@ export {
 	SearchIcon,
 	SingleSelect,
 	SingleSelectDumb,
+	Sidebar,
+	SidebarDumb,
 	SplitVertical,
 	StickySection,
 	SuccessIcon,
