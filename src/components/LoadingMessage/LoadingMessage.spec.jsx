fbe473c1c2812db9989b7589e91d69bd6db55d3b
Joe Moon <jmoon@appnexus.com>
2016-06-23 23:20:29 -0700

    rearrange tests to follow conventions

--- src/components/LoadingMessage/LoadingMessage.spec.jsx
+++ src/components/LoadingMessage/LoadingMessage.spec.jsx
@@ -13,9 +13,10 @@ const {
 } = LoadingMessage;
 
 describe('LoadingMessage', () => {
+
 	common(LoadingMessage);
 
-	describe('default', () => {
+	describe('render', () => {
 
 		let wrapper;
 
@@ -39,82 +40,86 @@ describe('LoadingMessage', () => {
 
 	});
 
-	describe('custom icon', () => {
+	describe('child components', () => {
 
-		it('should render the custom icon', () => {
-			const wrapper = shallow(
-				<LoadingMessage>
-					<Icon><SuccessIcon/></Icon>
-				</LoadingMessage>
-			);
-			assert.equal(wrapper.find(SuccessIcon).length, 1);
+		describe('Icon', () => {
+
+			it('should render the custom icon', () => {
+				const wrapper = shallow(
+					<LoadingMessage>
+						<Icon><SuccessIcon/></Icon>
+					</LoadingMessage>
+				);
+				assert.equal(wrapper.find(SuccessIcon).length, 1);
+			});
+
+			it('should render the custom icon', () => {
+				const wrapper = shallow(
+					<LoadingMessage Icon={<SuccessIcon/>}/>
+				);
+				assert.equal(wrapper.find(SuccessIcon).length, 1);
+			})
+
 		});
 
-		it('should render the custom icon', () => {
-			const wrapper = shallow(
-				<LoadingMessage Icon={<SuccessIcon/>}/>
-			);
-			assert.equal(wrapper.find(SuccessIcon).length, 1);
-		})
+		describe('Title', () => {
 
-	});
+			it('should render the custom title', () => {
+				const wrapper = shallow(
+					<LoadingMessage>
+						<Title>A custom title</Title>
+					</LoadingMessage>
+				);
+				assert.equal(wrapper.find('.lucid-LoadingMessage-title').text(), 'A custom title');
+			});
 
-	describe('custom title', () => {
+			it('should render the custom title', () => {
+				const wrapper = shallow(
+					<LoadingMessage Title='A custom title' />
+				);
+				assert.equal(wrapper.find('.lucid-LoadingMessage-title').text(), 'A custom title');
+			});
 
-		it('should render the custom title', () => {
-			const wrapper = shallow(
-				<LoadingMessage>
-					<Title>A custom title</Title>
-				</LoadingMessage>
-			);
-			assert.equal(wrapper.find('.lucid-LoadingMessage-title').text(), 'A custom title');
+			describe('null', () => {
+
+				let wrapper;
+
+				beforeEach(() => wrapper = shallow(<LoadingMessage Title={null}/>));
+
+				it('should render a LoadingIcon', () => {
+					assert.equal(wrapper.find(LoadingIcon).length, 1, 'must render loading icon');
+				});
+
+				it('should not render a title', () => {
+					assert.equal(wrapper.find('.lucid-LoadingMessage-title').length, 0);
+				});
+
+				it('should add `&-no-content` class', () => {
+					assert(wrapper.hasClass('lucid-LoadingMessage-no-content'));
+				});
+
+			});
+
 		});
 
-		it('should render the custom title', () => {
-			const wrapper = shallow(
-				<LoadingMessage Title='A custom title' />
-			);
-			assert.equal(wrapper.find('.lucid-LoadingMessage-title').text(), 'A custom title');
-		});
+		describe('Body', () => {
 
-	});
+			it('should render the custom body', () => {
+				const wrapper = shallow(
+					<LoadingMessage>
+						<Body>A custom body</Body>
+					</LoadingMessage>
+				);
+				assert.equal(wrapper.find('.lucid-LoadingMessage-body').text(), 'A custom body');
+			});
 
-	describe('custom body', () => {
+			it('should render the custom body', () => {
+				const wrapper = shallow(
+					<LoadingMessage Body='A custom body' />
+				);
+				assert.equal(wrapper.find('.lucid-LoadingMessage-body').text(), 'A custom body');
+			});
 
-		it('should render the custom body', () => {
-			const wrapper = shallow(
-				<LoadingMessage>
-					<Body>A custom body</Body>
-				</LoadingMessage>
-			);
-			assert.equal(wrapper.find('.lucid-LoadingMessage-body').text(), 'A custom body');
-		});
-
-		it('should render the custom body', () => {
-			const wrapper = shallow(
-				<LoadingMessage Body='A custom body' />
-			);
-			assert.equal(wrapper.find('.lucid-LoadingMessage-body').text(), 'A custom body');
-		});
-
-	});
-
-	describe('no title', () => {
-
-		let wrapper;
-
-		beforeEach(() => wrapper = shallow(<LoadingMessage Title={null}/>));
-
-		it('should render a LoadingIcon', () => {
-			assert.equal(wrapper.find(LoadingIcon).length, 1, 'must render loading icon');
-		});
-
-		it('should not render a title', () => {
-			assert.equal(wrapper.find('.lucid-LoadingMessage-title').length, 0);
-		});
-
-		it('should add `&-no-content` class', () => {
-			assert(wrapper.hasClass('lucid-LoadingMessage-no-content'));
 		});
 
 	});
