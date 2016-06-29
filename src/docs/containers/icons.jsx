2cd275529a9934ed38d77fa72c6f4d90f9f443c0
Joe Moon <joe@xoxomoon.com>
2016-06-29 12:04:08 -0700

    add Icons page

---
+++ src/docs/containers/icons.jsx
@@ -0,0 +1,49 @@
+import _ from 'lodash';
+import React from 'react';
+
+import { lucidClassNames } from '../../util/style-helpers';
+import * as components from '../../index';
+
+const { Button } = components;
+const icons = _.reduce(components, (acc, val, key) =>
+	_.assign(acc, _.endsWith(key, 'Icon') && key !== 'Icon'
+		? { [key]: val }
+		: {}
+	), {});
+
+const cx = lucidClassNames.bind('Icons');
+
+const Icons = React.createClass({
+
+	render() {
+
+		return (
+			<div className={cx('&', 'Component')}>
+				<h2>Icons</h2>
+
+				<p>A list of all lucid Icons.</p>
+
+				<section>
+					<ul className={cx('&-list')}>
+						{_.map(icons, (Icon, name) => (
+							<li key={name}>
+								<Button
+									kind='link'
+									onClick={() => this.props.router.push({
+										pathname: `/components/${name}`,
+										query: this.props.location.query,
+									})}
+								>
+									<Icon /> {name}
+								</Button>
+							</li>
+						))}
+					</ul>
+				</section>
+
+			</div>
+		);
+	},
+});
+
+export default Icons;
