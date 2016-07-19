5533355092a04c7431fbf5f6d7a6f92fedd11c8a
Jon de la Motte <jdelamotte@appnexus.com>
2016-07-19 13:21:01 -0700

    Fix some icons according to new svgs from dmaloney

--- src/components/Icon/DangerIcon/DangerIcon.jsx
+++ src/components/Icon/DangerIcon/DangerIcon.jsx
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 import React from 'react';
-import CrossIcon  from '../CrossIcon/CrossIcon';
+import Icon from '../Icon';
 import { lucidClassNames } from '../../../util/style-helpers';
 import { createClass, omitProps } from '../../../util/component-types';
 
@@ -15,7 +15,7 @@ const cx = lucidClassNames.bind('&-DangerIcon');
 const DangerIcon = createClass({
 	displayName: 'DangerIcon',
 	propTypes: {
-		...CrossIcon.propTypes,
+		...Icon.propTypes,
 	},
 
 	render() {
@@ -25,12 +25,14 @@ const DangerIcon = createClass({
 		} = this.props;
 
 		return (
-			<CrossIcon
+			<Icon
 				{...omitProps(passThroughs, DangerIcon)}
-				{..._.pick(passThroughs, _.keys(CrossIcon.propTypes))}
+				{..._.pick(passThroughs, _.keys(Icon.propTypes))}
 				className={cx('&', className)}
-				isBadge
-			/>
+			>
+				<circle cx='8' cy='8' r='8'/>
+				<path className={cx('&-background')} d='M6.836,8L4.387,5.537l1.17-1.171l2.449,2.465l2.465-2.465l1.17,1.171L9.163,8l2.479,2.465l-1.168,1.17 L8.007,9.156l-2.48,2.479l-1.17-1.171L6.836,8z'/>
+			</Icon>
 		);
 	},
 });
--- src/components/Icon/InfoIcon/InfoIcon.jsx
+++ src/components/Icon/InfoIcon/InfoIcon.jsx
@@ -29,10 +29,10 @@ const InfoIcon = createClass({
 				{...omitProps(passThroughs, InfoIcon)}
 				{..._.pick(passThroughs, _.keys(Icon.propTypes))}
 				className={cx('&', className)}
-				isBadge
 			>
-				<rect x='7' y='3' width='2' height='2'/>
-				<rect x='7' y='6' width='2' height='7'/>
+				<circle cx='8' cy='8' r='8'/>
+				<rect className={cx('&-background')} x='7' y='3' width='2' height='2'/>
+				<rect className={cx('&-background')} x='7' y='6' width='2' height='7'/>
 			</Icon>
 		);
 	},
