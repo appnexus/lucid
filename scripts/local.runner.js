59caa8bb1076a6872db8f459aeac7229209d735a
Joe Moon <jmoon@appnexus.com>
2016-07-28 22:48:12 -0700

    browserstack test build and server setup

--- scripts/local.runner.js
+++ scripts/local.runner.js
@@ -1,27 +1,50 @@
 #!/usr/bin/env node
 
+/*eslint no-console: "allow"*/
+
+const gulp = require('gulp');
+require('../gulpfile.js');
+const webpack = require('webpack');
+const compiler = webpack(require('../webpack.config.js'));
+const WebpackDevServer = require('webpack-dev-server');
 const Nightwatch = require('nightwatch');
 const browserstack = require('browserstack-local');
 
-// Code to start browserstack local before start of test
-console.log('Connecting local');
-const bs_local = new browserstack.Local();
-bs_local.start({'key': process.env.BROWSERSTACK_ACCESS_KEY }, function(error) {
-	if (error) {
-		console.error(error);
-		process.exit(1);
-	}
+const server = new WebpackDevServer(compiler, {
+	contentBase: '../src/docs/',
+	stats: 'errors-only',
+});
+const local = new browserstack.Local();
+const browserStackConfig = {
+	key: process.env.BROWSERSTACK_ACCESS_KEY,
+};
 
-	console.log('Connected. Now testing...');
-	Nightwatch.cli(function(argv) {
-		Nightwatch.CliRunner(argv)
-		.setup(null, function(){
-			// Code to stop browserstack local after end of parallel test
-			bs_local.stop(function(){});
-		})
-		.runTests(function(){
-			// Code to stop browserstack local after end of single test
-			bs_local.stop(function(){});
+console.log('generate docs');
+gulp.start('docs-build', () => {
+
+	console.log('start webpack server');
+	server.listen(8080, 'localhost', () => {
+
+		//Code to start browserstack local before start of test
+		console.log('connect local');
+		local.start(browserStackConfig, error => {
+
+			if (error) { return console.error(error); }
+			console.log('start test setup');
+			Nightwatch.cli(argv => {
+
+				Nightwatch.CliRunner(argv)
+					.setup(null, () => console.log('finished test setup'))
+					.runTests(() => {
+						console.log('run tests');
+						// Code to stop browserstack local after end of single test
+						local.stop(() => {
+							server.close()
+							process.exit(0);
+						});
+					});
+
+			});
 		});
 	});
 });
