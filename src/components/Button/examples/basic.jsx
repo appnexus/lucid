b41586e95775bd62f2f22a9dd612ce464494c2c4
Jon de la Motte <jdelamotte@appnexus.com>
2016-08-02 13:55:16 -0700

    Fixes #441 by adding `hasOnlyIcon` prop to `Button`

--- src/components/Button/examples/basic.jsx
+++ src/components/Button/examples/basic.jsx
@@ -8,21 +8,28 @@ export default React.createClass({
 				<article>
 					<Button>Default</Button>
 					<Button><CheckIcon /> has icon</Button>
+					<Button hasOnlyIcon={true}><CheckIcon /></Button>
 					<Button isDisabled={true}>is disabled</Button>
 					<Button isActive={true}>is active</Button>
 				</article>
 				<article>
 					<Button size='short'>short</Button>
+					<Button size='short'><CheckIcon /> has icon</Button>
+					<Button size='short' hasOnlyIcon={true}><CheckIcon /></Button>
 					<Button size='short' isDisabled={true}>short is disabled</Button>
 					<Button size='short' isActive={true}>short is active</Button>
 				</article>
 				<article>
 					<Button size='small'>small</Button>
+					<Button size='small'><CheckIcon /> has icon</Button>
+					<Button size='small' hasOnlyIcon={true}><CheckIcon /></Button>
 					<Button size='small' isDisabled={true}>small is disabled</Button>
 					<Button size='small' isActive={true}>small is active</Button>
 				</article>
 				<article>
 					<Button size='large'>large</Button>
+					<Button size='large'><CheckIcon /> has icon</Button>
+					<Button size='large' hasOnlyIcon={true}><CheckIcon /></Button>
 					<Button size='large'><CheckIcon /> large has icon</Button>
 					<Button size='large' isDisabled={true}>large is disabled</Button>
 					<Button size='large' isActive={true}>large is active</Button>
