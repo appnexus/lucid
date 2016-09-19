4587942bfba43c538e7fd1d961934e15d81015a6
Nathan Levin-Greenhaw <nathan@njlg.info>
2016-09-19 15:15:36 -0700

    fixing VerticalTabs example

--- src/components/VerticalTabs/examples/stateful-with-title-as-prop.jsx
+++ src/components/VerticalTabs/examples/stateful-with-title-as-prop.jsx
@@ -5,7 +5,7 @@ export default React.createClass({
 	render() {
 		return (
 			<div>
-				<VerticalTabs selectedIndex={2}>
+				<VerticalTabs>
 					<VerticalTabs.Tab Title='One'>One content</VerticalTabs.Tab>
 					<VerticalTabs.Tab Title='Two'>Two content</VerticalTabs.Tab>
 					<VerticalTabs.Tab Title='Three'>Three content</VerticalTabs.Tab>
