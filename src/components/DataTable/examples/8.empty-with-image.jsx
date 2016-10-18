89602c9cd5ea646d0fd179bc66a0cd629bdd568b
Natan LaFontaine <natanx@gmail.com>
2016-10-18 16:26:59 -0700

    Fixes broken examples.

--- src/components/DataTable/examples/8.empty-with-image.jsx
+++ src/components/DataTable/examples/8.empty-with-image.jsx
@@ -28,50 +28,50 @@ export default React.createClass({
 					<Body>
 						<img src='https://dummyimage.com/375x150/ff69/fff' />
 					</Body>
-					<DataTable.Column
-						field='id'
-						width={41}
-						align='center'
-						hasBorderLeft
-						hasBorderLeft
-						isSortable
-					>
-						ID
-					</DataTable.Column>
-
-					<DataTable.Column
-						field='first_name'
-						width={100}
-						hasBorderLeft
-					>
-						First
-					</DataTable.Column>
-
-					<DataTable.Column
-						field='last_name'
-						align='left'
-						width={100}
-						hasBorderRight
-					>
-						Last
-					</DataTable.Column>
-
-					<DataTable.Column
-						field='email'
-						align='center'
-					>
-						E-Mail
-					</DataTable.Column>
-
-					<DataTable.Column
-						field='occupation'
-						align='right'
-						width={100}
-						hasBorderLeft
-					>
-						Occupation
-					</DataTable.Column>
 				</EmptyStateWrapper>
+				<DataTable.Column
+					field='id'
+					width={41}
+					align='center'
+					hasBorderLeft
+					hasBorderLeft
+					isSortable
+				>
+					ID
+				</DataTable.Column>
+
+				<DataTable.Column
+					field='first_name'
+					width={100}
+					hasBorderLeft
+				>
+					First
+				</DataTable.Column>
+
+				<DataTable.Column
+					field='last_name'
+					align='left'
+					width={100}
+					hasBorderRight
+				>
+					Last
+				</DataTable.Column>
+
+				<DataTable.Column
+					field='email'
+					align='center'
+				>
+					E-Mail
+				</DataTable.Column>
+
+				<DataTable.Column
+					field='occupation'
+					align='right'
+					width={100}
+					hasBorderLeft
+				>
+					Occupation
+				</DataTable.Column>
 			</DataTable>
 		);
 	},
