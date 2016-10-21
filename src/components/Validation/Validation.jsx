1cf352a6ea27d1ec6757bb9263dc668d4cce526b
Joe Moon <joe@xoxomoon.com>
2016-10-21 15:20:50 -0700

    update validation documentation for `Error=true` case

--- src/components/Validation/Validation.jsx
+++ src/components/Validation/Validation.jsx
@@ -35,6 +35,9 @@ const Validation = createClass({
 		 * In most cases this will be a string, but it also accepts any valid React
 		 * element. If this is a falsey value, then no error message will be
 		 * displayed.
+		 *
+		 * If this is the literal `true`, it will add the `-is-error` class to the
+		 * wrapper div, but not render the `-error-content` `div`.
 		 */
 		Error: any,
 
