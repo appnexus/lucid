6293613fb96ebdcf9dfb1c8af82d2cff8ddb50e7
Aditya Balwani <aditya@buncee.com>
2016-10-31 19:42:04 -0400

    Added UploadIcon and AddURLIcon

---
+++ src/components/Icon/AddURLIcon/AddURLIcon.spec.js
@@ -0,0 +1,8 @@
+import { icons, common } from '../../../util/generic-tests';
+
+import AddURLIcon from './AddURLIcon';
+
+describe('AddURLIcon', () => {
+	common(AddURLIcon);
+	icons(AddURLIcon);
+});
