6a9080e0a33112ab2982fc7fc5ada02f73579c0f
Natan LaFontaine <natanx@gmail.com>
2016-11-01 16:59:00 -0700

    Testing using Firefox launcher on Travis.

--- karma.conf.js
+++ karma.conf.js
@@ -28,11 +28,6 @@ module.exports = function(config) {
 			'src/test-entry.js': ['webpack'],
 		},
 
-		browserStack: {
-			username: process.env.BROWSERSTACK_USERNAME,
-			accessKey: process.env.BROWSERSTACK_ACCESS_KEY,
-		},
-
 		customLaunchers: {
 			bs_chrome: {
 				base: 'BrowserStack',
@@ -140,7 +135,7 @@ module.exports = function(config) {
 
 		// start these browsers
 		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-		browsers: ['Chrome'],
+		browsers: ['Firefox'],
 
 		// Continuous Integration mode
 		// if true, Karma captures browsers, runs the tests and exits
