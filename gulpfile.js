c17181ca02ec0bba3e128389453587da91d10ce5
Tim Santeford <tsantef@gmail.com>
2016-11-21 09:26:36 -0800

    Upgraded eslint, added lint fix, and removed gulp-eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp');
 
 var build = require('./gulp/build');
-var lint = require('./gulp/lint');
 var docs = require('./gulp/docs');
 
 gulp.task('build-css', build.css);
@@ -9,5 +8,4 @@ gulp.task('build-js', build.js);
 gulp.task('docs-generate', docs.generate);
 gulp.task('docs-upload', ['docs-generate'], docs.upload);
 gulp.task('docs-build', ['docs-generate'], docs.build);
-gulp.task('lint', lint);
 gulp.task('preversion', ['build-css', 'build-js']);
