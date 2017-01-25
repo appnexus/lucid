f22b262d857a0fb55ea65601f50b1fad358fb1e7
Jon de la Motte <jdelamotte@appnexus.com>
2017-01-25 12:50:21 -0800

    Don't put files in `dist`, also check for existing files

--- gulp/build.js
+++ gulp/build.js
@@ -12,6 +12,30 @@ var path = require('path');
 
 var CONFIG = require('./config');
 
+function indexAst() {
+	const code = fs.readFileSync(path.join(CONFIG.SOURCE_DIR, 'index.js')).toString();
+	return babylon.parse(code, { sourceType: 'module' });
+}
+
+function imports() {
+	const importDeclarations = _.filter(indexAst().program.body, (node) => node.type === 'ImportDeclaration');
+	return _.flatMap(importDeclarations, (node) => {
+		const moduleId = node.source.value;
+
+		return _.map(node.specifiers, ((specifier) => ([
+			specifier.local.name,
+			moduleId,
+		])));
+	});
+}
+
+function exports() {
+	const exportNamedDeclarations = _.filter(indexAst().program.body, (node) => node.type === 'ExportNamedDeclaration');
+	return _.flatMap(exportNamedDeclarations, (node) => (
+		_.map(node.specifiers, (specifier) => specifier.exported.name))
+	);
+}
+
 module.exports = {
 	css: function() {
 		return gulp.src([CONFIG.LESS_ENTRY])
@@ -37,36 +61,42 @@ module.exports = {
 	},
 
 	jsPathImports: function() {
+		const importMap = _.fromPairs(imports());
+		const allExports = exports();
 
-		// Make sure the build dir exists
-		fs.mkdirSync(CONFIG.BUILD_DIR);
+		const checkFilesPromise = Promise.all(_.map(allExports, (exportName) => {
+			return new Promise((resolve, reject) => {
+				fs.stat(`${exportName}.js`, (err) => {
+					if (!err) {
+						return reject(new Error(`One of the exports (${exportName}) in index.js conflicts with a file you have on your file system (${exportName}.js). Unable to write path-import files.`));
+					}
+					return resolve();
+				});
+			});
+		}));
 
-		const code = fs.readFileSync(path.join(CONFIG.SOURCE_DIR, 'index.js')).toString();
-		const ast = babylon.parse(code, { sourceType: 'module' });
-
-		const importDeclarations = _.filter(ast.program.body, (node) => node.type === 'ImportDeclaration');
-		const exportNamedDeclarations = _.filter(ast.program.body, (node) => node.type === 'ExportNamedDeclaration');
-
-		const exports = _.flatMap(exportNamedDeclarations, (node) => (
-			_.map(node.specifiers, (specifier) => specifier.exported.name))
-		);
-		const imports = _.flatMap(importDeclarations, (node) => {
-			const moduleId = node.source.value;
-
-			return _.map(node.specifiers, ((specifier) => ([
-				specifier.local.name,
-				moduleId,
-			])));
-		});
-		const importMap = _.fromPairs(imports);
-
-		return Promise.all(_.map(exports, (exportName) => {
+		const writeFilesPromise = Promise.all(_.map(allExports, (exportName) => {
 			return new Promise((resolve, reject) => {
 				const exportCode = `export * from '${importMap[exportName]}';`;
 				// for some reason this doesn't pickup our .babelrc even though it's supposed to
 				const transpiledCode = babelCore.transform(exportCode, { presets: [ 'es2015' ] }).code;
 
-				fs.writeFile(path.join(CONFIG.BUILD_DIR, `${exportName}.js`), transpiledCode, (err) => {
+				fs.writeFile(`${exportName}.js`, transpiledCode, (err) => {
+					if (err) {
+						return reject(err);
+					}
+					return resolve();
+				});
+			});
+		}));
+
+		return checkFilesPromise.then(writeFilesPromise);
+	},
+
+	jsCleanPathImports: function() {
+		return Promise.all(_.map(exports(), (exportName) => {
+			return new Promise((resolve, reject) => {
+				fs.unlink(`${exportName}.js`, (err) => {
 					if (err) {
 						return reject(err);
 					}
@@ -75,5 +105,6 @@ module.exports = {
 			});
 		}));
 	},
+
 };
 
