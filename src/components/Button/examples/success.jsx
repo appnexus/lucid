a8365f7d794da25fe1372082fa1302129d519cdc
Chris Dopuch <cdopuch@appnexus.com>
2017-10-03 13:04:53 -0700

    830: moves example-specific styling for Button out of the docs/index.less and into the individual Button example files

--- src/components/Button/examples/success.jsx
+++ src/components/Button/examples/success.jsx
@@ -2,28 +2,42 @@ import React from 'react';
 import createClass from 'create-react-class';
 import { Button } from '../../../index';
 
+const sectionStyle = {
+	display: 'flex',
+	'flex-direction': 'column',
+};
+
+const articleStyle = {
+	display: 'flex',
+	margin: '5px 0',
+};
+
+const buttonStyle = {
+	'margin-right': '5px',
+};
+
 export default createClass({
 	render() {
 		return (
-			<section>
-				<article>
-					<Button kind="success">Success</Button>
+			<section style={sectionStyle}>
+				<article style={articleStyle}>
+					<Button style={buttonStyle} kind="success">Success</Button>
 					<Button kind="success" isDisabled={true}>Disabled</Button>
 				</article>
-				<article>
-					<Button kind="success" size="short">Short</Button>
+				<article style={articleStyle}>
+					<Button style={buttonStyle} kind="success" size="short">Short</Button>
 					<Button kind="success" size="short" isDisabled={true}>
 						Short disabled
 					</Button>
 				</article>
-				<article>
-					<Button kind="success" size="small">Small</Button>
+				<article style={articleStyle}>
+					<Button style={buttonStyle} kind="success" size="small">Small</Button>
 					<Button kind="success" size="small" isDisabled={true}>
 						Small disabled
 					</Button>
 				</article>
-				<article>
-					<Button kind="success" size="large">Large</Button>
+				<article style={articleStyle}>
+					<Button style={buttonStyle} kind="success" size="large">Large</Button>
 					<Button kind="success" size="large" isDisabled={true}>
 						Large disabled
 					</Button>
