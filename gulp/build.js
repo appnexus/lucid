0897fa7b1915315ef2aee1a7ca051a20c0129a78
Joe Moon <jmoon@appnexus.com>
2017-11-07 13:14:28 -0800

    fix build to exclude coverage files

--- gulp/build.js
+++ gulp/build.js
@@ -13,22 +13,29 @@ var path = require('path');
 var CONFIG = require('./config');
 
 function indexAst() {
-	const code = fs.readFileSync(path.join(CONFIG.SOURCE_DIR, 'index.js')).toString();
+	const code = fs
+		.readFileSync(path.join(CONFIG.SOURCE_DIR, 'index.js'))
+		.toString();
 	return babylon.parse(code, { sourceType: 'module' });
 }
 
 function imports() {
-	const importDeclarations = _.filter(indexAst().program.body, (node) => node.type === 'ImportDeclaration');
-	return _.flatMap(importDeclarations, (node) => {
+	const importDeclarations = _.filter(
+		indexAst().program.body,
+		node => node.type === 'ImportDeclaration'
+	);
+	return _.flatMap(importDeclarations, node => {
 		const moduleId = node.source.value;
 		// Slightly clever way to detect if we're doing a path import vs a module
 		// import. `import x from './foo'` vs `import _ from 'lodash'`
 		const isPath = path.basename(moduleId) !== moduleId;
 		// If we're doing a path import, we need to make the import relative to the
 		// root directory instead of `dist`
-		const newPath = isPath ? './' + path.join(CONFIG.BUILD_DIR, moduleId) : null;
+		const newPath = isPath
+			? './' + path.join(CONFIG.BUILD_DIR, moduleId)
+			: null;
 
-		return _.map(node.specifiers, ((specifier) => {
+		return _.map(node.specifiers, specifier => {
 			return [
 				specifier.local.name,
 				{
@@ -36,14 +43,17 @@ function imports() {
 					path: newPath || moduleId,
 				},
 			];
-		}));
+		});
 	});
 }
 
 function exports() {
-	const exportNamedDeclarations = _.filter(indexAst().program.body, (node) => node.type === 'ExportNamedDeclaration');
-	return _.flatMap(exportNamedDeclarations, (node) => (
-		_.map(node.specifiers, (specifier) => specifier.exported.name))
+	const exportNamedDeclarations = _.filter(
+		indexAst().program.body,
+		node => node.type === 'ExportNamedDeclaration'
+	);
+	return _.flatMap(exportNamedDeclarations, node =>
+		_.map(node.specifiers, specifier => specifier.exported.name)
 	);
 }
 
@@ -66,31 +76,37 @@ function exportCode(specifierType, specifierPath, exportName) {
 	};
 
 	// For some reason this doesn't pickup our .babelrc even though it's supposed to
-	return babelCore.transform(codeMap[specifierType], { presets: [ 'es2015' ] }).code;
+	return babelCore.transform(codeMap[specifierType], { presets: ['es2015'] })
+		.code;
 }
 
 module.exports = {
 	css: function() {
-		return gulp.src([CONFIG.LESS_ENTRY])
-		.pipe(less())
-		.pipe(postcss([
-			autoprefixer({
-				browsers: [CONFIG.AUTOPREFIXER_BROWSERS],
-			}),
-		]))
-		.pipe(gulp.dest(CONFIG.BUILD_DIR));
+		return gulp
+			.src([CONFIG.LESS_ENTRY])
+			.pipe(less())
+			.pipe(
+				postcss([
+					autoprefixer({
+						browsers: [CONFIG.AUTOPREFIXER_BROWSERS],
+					}),
+				])
+			)
+			.pipe(gulp.dest(CONFIG.BUILD_DIR));
 	},
 
 	js: function() {
-		return gulp.src([
-			'!/**/*.json',
-			'!' + CONFIG.TEST_GLOB.SOURCE,
-			'!' + CONFIG.EXAMPLES_GLOB.SOURCE,
-			CONFIG.JS_GLOB.SOURCE,
-		])
-		.pipe(cache('build-js')) // only useful when using a watch task
-		.pipe(babel())
-		.pipe(gulp.dest(CONFIG.BUILD_DIR));
+		return gulp
+			.src([
+				'!/**/*.json',
+				'!/**/*.html',
+				'!' + CONFIG.TEST_GLOB.SOURCE,
+				'!' + CONFIG.EXAMPLES_GLOB.SOURCE,
+				CONFIG.JS_GLOB.SOURCE,
+			])
+			.pipe(cache('build-js')) // only useful when using a watch task
+			.pipe(babel())
+			.pipe(gulp.dest(CONFIG.BUILD_DIR));
 	},
 
 	/**
@@ -110,48 +126,60 @@ module.exports = {
 		const importMap = _.fromPairs(imports());
 		const allExports = exports();
 
-		const checkFilesPromise = Promise.all(_.map(allExports, (exportName) => {
-			return new Promise((resolve, reject) => {
-				fs.stat(`${exportName}.js`, (err) => {
-					if (!err) {
-						return reject(new Error(`One of the exports (${exportName}) in index.js conflicts with a file you have on your file system (${exportName}.js). Unable to write path-import files.`));
-					}
-					return resolve();
+		const checkFilesPromise = Promise.all(
+			_.map(allExports, exportName => {
+				return new Promise((resolve, reject) => {
+					fs.stat(`${exportName}.js`, err => {
+						if (!err) {
+							return reject(
+								new Error(
+									`One of the exports (${exportName}) in index.js conflicts with a file you have on your file system (${exportName}.js). Unable to write path-import files.`
+								)
+							);
+						}
+						return resolve();
+					});
 				});
-			});
-		}));
+			})
+		);
 
-		const writeFilesPromise = Promise.all(_.map(allExports, (exportName) => {
-			return new Promise((resolve, reject) => {
-				const specifierPath = importMap[exportName].path;
-				const specifierType = importMap[exportName].type;
+		const writeFilesPromise = Promise.all(
+			_.map(allExports, exportName => {
+				return new Promise((resolve, reject) => {
+					const specifierPath = importMap[exportName].path;
+					const specifierType = importMap[exportName].type;
 
-				const transpiledCode = exportCode(specifierType, specifierPath, exportName);
+					const transpiledCode = exportCode(
+						specifierType,
+						specifierPath,
+						exportName
+					);
 
-				fs.writeFile(`${exportName}.js`, transpiledCode, (err) => {
-					if (err) {
-						return reject(err);
-					}
-					return resolve();
+					fs.writeFile(`${exportName}.js`, transpiledCode, err => {
+						if (err) {
+							return reject(err);
+						}
+						return resolve();
+					});
 				});
-			});
-		}));
+			})
+		);
 
 		return checkFilesPromise.then(writeFilesPromise);
 	},
 
 	jsCleanPathImports: function() {
-		return Promise.all(_.map(exports(), (exportName) => {
-			return new Promise((resolve, reject) => {
-				fs.unlink(`${exportName}.js`, (err) => {
-					if (err) {
-						return reject(err);
-					}
-					return resolve();
+		return Promise.all(
+			_.map(exports(), exportName => {
+				return new Promise((resolve, reject) => {
+					fs.unlink(`${exportName}.js`, err => {
+						if (err) {
+							return reject(err);
+						}
+						return resolve();
+					});
 				});
-			});
-		}));
+			})
+		);
 	},
-
 };
-
