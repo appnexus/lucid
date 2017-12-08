014c6418d503d1c170a8d17ea3adafe2dc04798a
Oliver Gupte <ogupte@appnexus.com>
2017-12-07 16:35:12 -0800

    Add new icons: AudioIcon, TextIcon, and VideoIcon

---
+++ src/components/Icon/TextIcon/examples/2.clickable.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import createClass from 'create-react-class';
+import { TextIcon } from '../../../../index';
+
+export default createClass({
+	render() {
+		return <TextIcon isClickable />;
+	},
+});
