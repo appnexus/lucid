3796ba029c98b130efb9eab2daf3f7f90d3f3967
alisonailea <alisonstump@gmail.com>
2017-12-14 14:11:45 -0800

    851: update styles with Rachel

--- src/components/IconBox/IconBox.spec.jsx
+++ src/components/IconBox/IconBox.spec.jsx
@@ -3,6 +3,7 @@ import { common, controls } from '../../util/generic-tests';
 import IconBox from './IconBox';
 
 describe('IconBox', () => {
+	debugger;
 	common(IconBox);
 	controls(IconBox, {
 		callbackName: 'onClick',
