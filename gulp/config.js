1bababa6c8d9e324e1efda61d5d0a8cccfadfd9a
Oliver Gupte <ogupte@appnexus.com>
2018-04-09 17:04:11 -0700

    Remove dependency on gulp for path imports

--- gulp/config.js
+++
@@ -1,7 +0,0 @@
-const SOURCE_DIR = 'src';
-const BUILD_DIR = 'dist';
-
-module.exports = {
-	SOURCE_DIR: SOURCE_DIR,
-	BUILD_DIR: BUILD_DIR,
-};
--- gulpfile.js
+++
@@ -1,5 +0,0 @@
-const gulp = require('gulp');
-const build = require('./gulp/build');
-
-gulp.task('build-js-path-imports', build.jsPathImports);
-gulp.task('build-js-clean-path-imports', build.jsCleanPathImports);
