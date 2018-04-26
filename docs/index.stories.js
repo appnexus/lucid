4eb86334cfaf90a047f4f94879b4cad2ee36ddd5
Oliver Gupte <ogupte@appnexus.com>
2018-04-26 00:32:29 -0700

    Add retina display github ribbon to docs

--- docs/index.stories.js
+++ docs/index.stories.js
@@ -123,6 +123,7 @@ class ArticlePage extends React.Component {
 							border: 0,
 						}}
 						src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67"
+						srcSet="http://aral.github.io/fork-me-on-github-retina-ribbons/right-graphite@2x.png 2x"
 						alt="Fork me on GitHub"
 						data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"
 					/>
