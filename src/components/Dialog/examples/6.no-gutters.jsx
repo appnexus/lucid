8be2b701b3877e5ba25a6a44bd0bb4c3ee633325
Lilie de la Motte <liliedelamotte@gmail.com>
2018-05-24 14:11:19 -0700

    no gutters example added to Dialog component examples

---
+++ src/components/Dialog/examples/6.no-gutters.jsx
@@ -0,0 +1,37 @@
+import _ from 'lodash';
+import React from 'react';
+import createClass from 'create-react-class';
+import { Button, Dialog } from '../../../index';
+
+export default createClass({
+	getInitialState() {
+		return {
+			isShown: false,
+		};
+	},
+
+	handleShow(isShown) {
+		this.setState({ isShown });
+	},
+
+	render() {
+		return (
+			<div>
+				<Button onClick={_.partial(this.handleShow, !this.state.isShown)}>
+					Toggle
+				</Button>
+
+				<Dialog
+					isShown={this.state.isShown}
+					onBackgroundClick={_.partial(this.handleShow, false)}
+					onEscape={_.partial(this.handleShow, false)}
+					Header="Header"
+					size="medium"
+					hasGutters={false}
+				>
+					This `Dialog` has no gutters!
+				</Dialog>
+			</div>
+		);
+	},
+});
