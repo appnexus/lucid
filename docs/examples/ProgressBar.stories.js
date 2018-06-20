9c4de2c41cb29fb919a449905b8cd741b5180d13
Lilie de la Motte <liliedelamotte@gmail.com>
2018-06-20 10:42:32 -0700

    changes ProgressBar height and removes hasLabel example

--- docs/examples/ProgressBar.stories.js
+++ docs/examples/ProgressBar.stories.js
@@ -6,8 +6,6 @@ import WithDefaultsExample from './ProgressBar/WithDefaults';
 import withDefaultsCode from '!!raw-loader!./ProgressBar/WithDefaults';
 import VariousColorsExample from './ProgressBar/VariousColors';
 import variousColorsCode from '!!raw-loader!./ProgressBar/VariousColors';
-import HasLabelExample from './ProgressBar/HasLabel';
-import hasLabelCode from '!!raw-loader!./ProgressBar/HasLabel';
 
 storiesOf('ProgressBar', module)
 	.add(
@@ -26,16 +24,6 @@ storiesOf('ProgressBar', module)
 			component: ProgressBar,
 			example: VariousColorsExample,
 			code: variousColorsCode,
-			path: ['ProgressBar'],
-			options: { showAddonPanel: true },
-		})
-	)
-	.add(
-		'with label',
-		exampleStory({
-			component: ProgressBar,
-			example: HasLabelExample,
-			code: hasLabelCode,
 			path: ['ProgressBar'],
 			options: { showAddonPanel: true },
 		})
