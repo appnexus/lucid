f12b26557ffac949599890ce75a16e2761b1d0af
Lilie de la Motte <liliedelamotte@gmail.com>
2018-07-19 12:02:51 -0700

    adds notes to developers regarding margin values

--- src/components/Axis/examples/8.diagonal-text.jsx
+++ src/components/Axis/examples/8.diagonal-text.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import createClass from 'create-react-class';
 import { Axis, d3Scale } from '../../../index';
 
+// individual margin values may need to be changed to
+// prevent or add extra padding depending on use of axis.
 const margin = { right: 30, left: 20, top: 40, bottom: 10 };
 const horizontalAxisWidth = 500;
 const horizontalAxisHeight = 100;
