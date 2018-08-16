44f998020a4fa80260d63f48317a8eba07241fd8
Lilie de la Motte <liliedelamotte@gmail.com>
2018-08-16 14:09:08 -0700

    got button with icon working, attached it to .less

--- docs/examples/ButtonWithIcon/WithDefaults.js
+++ docs/examples/ButtonWithIcon/WithDefaults.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { ButtonWithIcon } from '../../../src/index.js';
-import { CheckIcon } from '../../../src/components/Icon/CheckIcon/CheckIcon.jsx';
+import EditPageIcon from '../../../src/components/Icon/EditPageIcon/EditPageIcon';
 
 export default class extends React.Component {
 	render() {
-		return <ButtonWithIcon Icon={CheckIcon} />;
+		return <ButtonWithIcon icon={<EditPageIcon />} />;
 	}
 }
--- src/components/ButtonWithIcon/ButtonWithIcon.jsx
+++ src/components/ButtonWithIcon/ButtonWithIcon.jsx
@@ -9,7 +9,7 @@ import { createClass } from '../../util/component-types';
 
 const cx = lucidClassNames.bind('&-ButtonWithIcon');
 
-const { any, string, oneOfType, node, arrayOf } = PropTypes;
+const { element, string, oneOfType, node, arrayOf } = PropTypes;
 
 const ButtonWithIcon = createClass({
 	displayName: 'ButtonWithIcon',
@@ -42,42 +42,17 @@ const ButtonWithIcon = createClass({
 			Form element type variations of Button. Passed through to DOM Button.
 		`,
 
-		Icon: any`
+		icon: element`
 			The \`Icon\` displayed inside the Button.
 		`,
-	},
-
-	components: {
-		Icon: createClass({
-			displayName: 'LoadingMessage.Icon',
-			statics: {
-				peek: {
-					description: `
-						Renders the \`Icon\` element passed in
-					`,
-				},
-			},
-			propName: 'Icon',
-		}),
-
-		Button: createClass({
-			displayName: 'ButtonGroup.Button',
-			statics: {
-				peek: {
-					description: `
-						Renders a \`<Button\`> inside the \`ButtonGroup\`.
-					`,
-				},
-			},
-		}),
 	},
 
 	getDefaultProps() {
 		return {
 			isDisabled: false,
 			isActive: false,
-			iconIsOnLeft: true,
 			onClick: _.noop,
+			icon: null,
 			type: 'button',
 		};
 	},
@@ -102,20 +77,23 @@ const ButtonWithIcon = createClass({
 			className,
 			children,
 			type,
+			icon,
 			...passThroughs
 		} = this.props;
 
 		return (
-			<Button
-				className={cx('&-Button', className)}
-				isDisabled={isDisabled}
-				onClick={this.handleClick}
-				ref="button"
-				type={type}
-			>
-				{this.props.Icon}
-				Here's a button!
-			</Button>
+			<div>
+				<Button
+					className={cx('&-Button', className)}
+					isDisabled={isDisabled}
+					onClick={this.handleClick}
+					ref="button"
+					type={type}
+				>
+					<span className={cx('&-icon')}>{icon}</span>
+					Button with an Icon
+				</Button>
+			</div>
 		);
 	},
 });
