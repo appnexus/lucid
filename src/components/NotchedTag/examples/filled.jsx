ed435ae86b92a6abbaa57c11d68846af85589be3
Kyle McGuire <kmcguire@appnexus.com>
2019-04-25 12:04:50 -0700

    update notchedTags to latest designs

--- src/components/NotchedTag/examples/filled.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import createClass from 'create-react-class';
-import { NotchedTag } from '../../../index';
-
-export default createClass({
-	render() {
-		return <NotchedTag type="filled">HI</NotchedTag>;
-	},
-});
