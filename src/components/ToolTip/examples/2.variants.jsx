745c54fe4eec0028e4c8cea078a16bac0a4242a5
Jack Toumey <jack@toumey.io>
2019-04-26 14:07:38 -0700

    updates to tooltip

--- src/components/ToolTip/examples/2.variants.jsx
+++ src/components/ToolTip/examples/2.variants.jsx
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React from 'react';
 import createClass from 'create-react-class';
 import { ToolTipDumb } from '../../../index';
@@ -8,76 +9,121 @@ export default createClass({
 	getInitialState: () => ({ isExpanded: true }),
 	render() {
 		return (
-			<section style={{ marginTop: 60, marginBottom: 60 }}>
-				<h4>No Title or Close Button</h4>
-				<div style={{ marginTop: 60, marginBottom: 60 }}>
-					<ToolTipDumb direction="right" isExpanded={this.state.isExpanded}>
-						<Body>
-							ToolTip is a utility component to create a transient message
-							anchored to another component.
-						</Body>
-						<Target>
-							<div>Target</div>
-						</Target>
-					</ToolTipDumb>
-				</div>
+			<section
+				style={{
+					display: 'flex',
+					flexDirection: 'column',
+				}}
+			>
+				<section
+					style={{
+						marginTop: 150,
+						display: 'flex',
+						flexDirection: 'row',
+						justifyContent: 'space-around',
+					}}
+				>
+					<div style={{ marginTop: 60, marginBottom: 60 }}>
+						<ToolTipDumb isExpanded={this.state.isExpanded}>
+							<Body>
+								ToolTip is a utility component to create a transient message
+								anchored to another component.
+							</Body>
+							<Target>
+								<div>No Title or Close Button</div>
+							</Target>
+						</ToolTipDumb>
+					</div>
 
-				<h4>With Close Button</h4>
-				<div style={{ marginTop: 60, marginBottom: 60 }}>
-					<ToolTipDumb
-						direction="right"
-						isCloseable
-						onClose={() => this.setState({ isExpanded: false })}
-						isExpanded={this.state.isExpanded}
-					>
-						<Body>
-							ToolTip is a utility component to create a transient message
-							anchored to another component.
-						</Body>
-						<Target>
-							<div>Target</div>
-						</Target>
-					</ToolTipDumb>
-				</div>
+					<div style={{ marginTop: 60, marginBottom: 60 }}>
+						<ToolTipDumb
+							isCloseable
+							onClose={() => this.setState({ isExpanded: false })}
+							isExpanded={this.state.isExpanded}
+						>
+							<Body>
+								ToolTip is a utility component to create a transient message
+								anchored to another component.
+							</Body>
+							<Target>
+								<div>With Close Button</div>
+							</Target>
+						</ToolTipDumb>
+					</div>
 
-				<h4>With Title and Close Button</h4>
-				<div style={{ marginTop: 60, marginBottom: 60 }}>
-					<ToolTipDumb
-						direction="right"
-						isCloseable
-						onClose={() => this.setState({ isExpanded: false })}
-						isExpanded={this.state.isExpanded}
-					>
-						<Title>Title</Title>
-						<Body>
-							ToolTip is a utility component to create a transient message
-							anchored to another component.
-						</Body>
-						<Target>
-							<div>Target</div>
-						</Target>
-					</ToolTipDumb>
-				</div>
+					<div style={{ marginTop: 60, marginBottom: 60 }}>
+						<ToolTipDumb
+							isCloseable
+							onClose={() => this.setState({ isExpanded: false })}
+							isExpanded={this.state.isExpanded}
+						>
+							<Title>Title</Title>
+							<Body>
+								ToolTip is a utility component to create a transient message
+								anchored to another component.
+							</Body>
+							<Target>
+								<div>With Title and Close Button</div>
+							</Target>
+						</ToolTipDumb>
+					</div>
+				</section>
 
-				<h4>With Title and Close Button (Light Version)</h4>
-				<div style={{ marginTop: 60, marginBottom: 60 }}>
-					<ToolTipDumb
-						direction="right"
-						isLight
-						isCloseable
-						onClose={() => this.setState({ isExpanded: false })}
-						isExpanded={this.state.isExpanded}
-					>
-						<Title>Title</Title>
-						<Body>
-							ToolTip is a utility component to create a transient message
-							anchored to another component.
-						</Body>
-						<Target>
-							<div>Target</div>
-						</Target>
-					</ToolTipDumb>
-				</div>
+				<section
+					style={{
+						marginTop: 150,
+						display: 'flex',
+						flexDirection: 'row',
+						justifyContent: 'space-around',
+					}}
+				>
+					<div style={{ marginTop: 60, marginBottom: 60 }}>
+						<ToolTipDumb isLight isExpanded={this.state.isExpanded}>
+							<Body>
+								ToolTip is a utility component to create a transient message
+								anchored to another component.
+							</Body>
+							<Target>
+								<div>No Title or Close Button</div>
+							</Target>
+						</ToolTipDumb>
+					</div>
+
+					<div style={{ marginTop: 60, marginBottom: 60 }}>
+						<ToolTipDumb
+							isLight
+							isCloseable
+							onClose={() => this.setState({ isExpanded: false })}
+							isExpanded={this.state.isExpanded}
+						>
+							<Body>
+								ToolTip is a utility component to create a transient message
+								anchored to another component.
+							</Body>
+							<Target>
+								<div>With Close Button</div>
+							</Target>
+						</ToolTipDumb>
+					</div>
+
+					<div style={{ marginTop: 60, marginBottom: 60 }}>
+						<ToolTipDumb
+							isLight
+							isCloseable
+							onClose={() => this.setState({ isExpanded: false })}
+							isExpanded={this.state.isExpanded}
+						>
+							<Title>Title</Title>
+							<Body>
+								ToolTip is a utility component to create a transient message
+								anchored to another component.
+							</Body>
+							<Target>
+								<div>With Title and Close Button</div>
+							</Target>
+						</ToolTipDumb>
+					</div>
+				</section>
 			</section>
 		);
 	},
