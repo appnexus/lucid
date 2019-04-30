36aa3e2333e512d84be9cbb0967dd29bacfed330
Jack Toumey <jack@toumey.io>
2019-04-30 12:08:11 -0700

    default split button meets specs

--- src/components/SplitButton/examples/01.default.jsx
+++ src/components/SplitButton/examples/01.default.jsx
@@ -7,37 +7,13 @@ export default createClass({
 		return (
 			<div style={{ height: 100 }}>
 				<SplitButton style={{ marginRight: '20px' }}>
-					<SplitButton.Button>Primary</SplitButton.Button>
+					<SplitButton.Button>Basic</SplitButton.Button>
 					<SplitButton.Button>One</SplitButton.Button>
 					<SplitButton.Button>Two</SplitButton.Button>
 				</SplitButton>
 
 				<SplitButton kind="primary" style={{ marginRight: '20px' }}>
 					<SplitButton.Button kind="primary">Primary</SplitButton.Button>
-					<SplitButton.Button>One</SplitButton.Button>
-					<SplitButton.Button>Two</SplitButton.Button>
-				</SplitButton>
-
-				<SplitButton kind="success" style={{ marginRight: '20px' }}>
-					<SplitButton.Button kind="success">Success</SplitButton.Button>
-					<SplitButton.Button>One</SplitButton.Button>
-					<SplitButton.Button>Two</SplitButton.Button>
-				</SplitButton>
-
-				<SplitButton kind="warning" style={{ marginRight: '20px' }}>
-					<SplitButton.Button>Warning</SplitButton.Button>
-					<SplitButton.Button>One</SplitButton.Button>
-					<SplitButton.Button>Two</SplitButton.Button>
-				</SplitButton>
-
-				<SplitButton kind="danger" style={{ marginRight: '20px' }}>
-					<SplitButton.Button kind="danger">Danger</SplitButton.Button>
-					<SplitButton.Button>One</SplitButton.Button>
-					<SplitButton.Button>Two</SplitButton.Button>
-				</SplitButton>
-
-				<SplitButton kind="info" style={{ marginRight: '20px' }}>
-					<SplitButton.Button kind="info">Info</SplitButton.Button>
 					<SplitButton.Button>One</SplitButton.Button>
 					<SplitButton.Button>Two</SplitButton.Button>
 				</SplitButton>
