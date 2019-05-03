171a860a0fc3c10a076b7815114bdc8993f90cd1
Jack Toumey <jack@toumey.io>
2019-05-03 11:10:51 -0700

    linting stuff

--- src/components/DataTable/examples/04.interactive-table.jsx
+++ src/components/DataTable/examples/04.interactive-table.jsx
@@ -177,8 +177,10 @@ export default createClass({
 
 		return (
 			<DataTable
-				data={_.map(data, (row, index) =>
-					index === activeIndex ? { ...row, isActive: true } : row
+				data={_.map(
+					data,
+					(row, index) =>
+						index === activeIndex ? { ...row, isActive: true } : row
 				)}
 				density="extended"
 				isSelectable
