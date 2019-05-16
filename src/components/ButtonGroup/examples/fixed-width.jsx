0244a7cb04af3e7ac40c8d2985c357ed1af91a0e
Jack <jack@toumey.io>
2019-05-15 17:02:24 -0700

    adding fixed width example

---
+++ src/components/ButtonGroup/examples/fixed-width.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import createClass from 'create-react-class';
+import { ButtonGroupDumb as ButtonGroup } from '../../../index';
+
+export default createClass({
+	render() {
+		const buttonStyle = { width: '100px' };
+		return (
+			<ButtonGroup selectedIndices={[7, 8]}>
+				<ButtonGroup.Button style={buttonStyle}>Smol</ButtonGroup.Button>
+				<ButtonGroup.Button style={buttonStyle}>Lonnnnnnnng</ButtonGroup.Button>
+				<ButtonGroup.Button style={buttonStyle}>Medium</ButtonGroup.Button>
+			</ButtonGroup>
+		);
+	},
+});
