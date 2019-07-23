e9dc78b829425101f6e2a30253da7304eb753f24
Jack Toumey <jack@toumey.io>
2019-07-23 16:07:11 -0700

    upgrading webpack config to return a function

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -6,36 +6,43 @@
 // When you add this file, we won't add the default configurations which is similar
 // to "React Create App". This only has babel loader to load JavaScript.
 
-module.exports = {
-	mode: 'production',
-	devtool: 'cheap-module-eval-source-map',
-	plugins: [
-		// your custom plugins
-	],
-	resolve: {
-		extensions: ['.js', '.jsx', '.ts', '.tsx'],
-	},
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				loader: 'ts-loader',
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.less$/,
-				use: [
-					{
-						loader: 'style-loader',
-					},
-					{
-						loader: 'css-loader',
-					},
-					{
-						loader: 'less-loader',
-					},
-				],
-			},
-		],
-	},
+module.exports = ({ config, mode }) => {
+
+	config = {
+		...config,
+		mode: 'production',
+		devtool: '#cheap-module-source-map',
+		resolve: {
+			...config.resolve,
+			extensions: ['.js', '.jsx', '.ts', '.tsx'],
+		},
+		module: {
+			...config.module,
+			rules: [
+				...config.module.rules,
+				{
+					test: /\.tsx?$/,
+					loader: 'ts-loader',
+					exclude: /node_modules/,
+				},
+				{
+					test: /\.less$/,
+					use: [
+						{
+							loader: 'style-loader',
+						},
+						{
+							loader: 'css-loader',
+						},
+						{
+							loader: 'less-loader',
+						},
+					],
+				},
+			],
+		},
+
+	}
+
+	return config;
 };
