cf3db4aa9264b8a9a5cadc2bcddc4d6ae5202dcd
Kris Hoyt <khoyt@appnexus.com>
2019-07-31 13:50:27 -0700

    Add component to render a drag and drop list

--- src/index.js
+++ src/index.js
@@ -130,6 +130,7 @@ import QuestionMarkIcon from './components/Icon/QuestionMarkIcon/QuestionMarkIco
 import RadioButton from './components/RadioButton/RadioButton';
 import RadioButtonLabeled from './components/RadioButtonLabeled/RadioButtonLabeled';
 import RefreshIcon from './components/Icon/RefreshIcon/RefreshIcon';
+import DraggableList from './components/DraggableList/DraggableList';
 import ResizeIcon from './components/Icon/ResizeIcon/ResizeIcon';
 import Resizer from './components/Resizer/Resizer';
 import ScrollTable from './components/ScrollTable/ScrollTable';
@@ -299,6 +300,7 @@ export {
 	RadioGroup,
 	RadioGroupDumb,
 	RefreshIcon,
+	DraggableList,
 	ResizeIcon,
 	Resizer,
 	ScrollTable,
