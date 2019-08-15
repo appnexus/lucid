daa2313f290823647f3f2e286912b2d60503c8d1
Dan Susskind <dan.susskind@xandr.com>
2019-08-15 12:36:05 -0700

    convert Button and Validation to functional components

--- src/components/Validation/Validation.tsx
+++ src/components/Validation/Validation.tsx
@@ -1,33 +1,33 @@
 import React from 'react';
 import PropTypes from 'react-peek/prop-types';
 import { lucidClassNames } from '../../util/style-helpers';
-import { getFirst, omitProps } from '../../util/component-types';
+import { getFirst, omitProps, FC } from '../../util/component-types';
 import _ from 'lodash';
 
 const cx = lucidClassNames.bind('&-Validation');
 
-const { any } = PropTypes;
+const { string, any } = PropTypes;
 
 interface IValidationErrorProps {
 	description?: string;
 	children?: React.ReactNode;
 }
-class ValidationError extends React.Component<IValidationErrorProps, {}, {}> {
-	constructor(props: IValidationErrorProps) {
-		super(props);
-	}
-	static displayName = 'Validation.Error';
-	static peek = {
-		description: `
-			Content that will be displayed as an error message.
-		`,
-	};
-	static propName = 'Error';
 
-	render(): React.ReactNode {
-		return <div>{this.props.children}</div>;
-	}
-}
+const ValidationError: FC<IValidationErrorProps> = (
+	props
+): React.ReactElement => <div>{props.children}</div>;
+
+ValidationError.displayName = 'Validation.Error';
+ValidationError.peek = {
+	description: `
+		Content that will be displayed as an error message.
+	`,
+};
+ValidationError.propName = 'Error';
+ValidationError.propTypes = {
+	description: string,
+	children: any,
+};
 
 export interface IValidationProps {
 	/** Any valid React children. */
@@ -44,77 +44,76 @@ export interface IValidationProps {
 	Error?: string | React.ReactNode & { props: IValidationErrorProps };
 }
 
-class Validation extends React.Component<IValidationProps, {}, {}> {
-	constructor(props: IValidationProps) {
-		super(props);
-	}
-	static displayName = 'Validation';
-	static peek = {
-		description: `
-			Validation is a wrapper component that's meant to be used by other
-			components. Wrap your form components in it and style them accordingly
-			if there's an error.
-		`,
-		categories: ['helpers'],
-	};
-	static propTypes = {
-		Error: any`
-			In most cases this will be a string, but it also accepts any valid React
-			element. If this is a falsey value, then no error message will be
-			displayed.  If this is the literal \`true\`, it will add the
-			\`-is-error\` class to the wrapper div, but not render the
-			\`-error-content\` \`div\`.
-		`,
-
-		className: any`
-			Classes that are appended to the component defaults. This prop is run
-			through the \`classnames\` library.
-		`,
-
-		children: any.isRequired`
-			Any valid React children.
-		`,
-	};
-
-	static Error = ValidationError;
-
-	render(): React.ReactNode {
-		const { className, children, ...passThroughs } = this.props;
-
-		const errorChildProps = _.get(
-			getFirst<IValidationErrorProps>(this.props, Validation.Error),
-			'props'
-		);
-
-		return (
-			<div
-				{...omitProps<IValidationProps>(
-					passThroughs,
-					undefined,
-					Object.keys(Validation.propTypes)
-				)}
-				className={cx(
-					'&',
-					{
-						'&-is-error': errorChildProps && errorChildProps.children,
-					},
-					className
-				)}
-			>
-				{children}
-				{errorChildProps &&
-				errorChildProps.children &&
-				errorChildProps.children !== true ? (
-					<div
-						{...omitProps<IValidationProps>(errorChildProps, undefined)}
-						className={cx('&-error-content', errorChildProps.className)}
-					>
-						{errorChildProps.children}
-					</div>
-				) : null}
-			</div>
-		);
-	}
+interface IValidationFC extends FC<IValidationProps> {
+	Error: ValidationError;
 }
+
+const Validation: IValidationFC = (props): React.ReactElement => {
+	const { className, children, ...passThroughs } = props;
+
+	const errorChildProps = _.get(
+		getFirst<IValidationErrorProps>(props, Validation.Error),
+		'props'
+	);
+
+	return (
+		<div
+			{...omitProps<IValidationProps>(
+				passThroughs,
+				undefined,
+				_.keys(Validation.propTypes)
+			)}
+			className={cx(
+				'&',
+				{
+					'&-is-error': errorChildProps && errorChildProps.children,
+				},
+				className
+			)}
+		>
+			{children}
+			{errorChildProps &&
+			errorChildProps.children &&
+			errorChildProps.children !== true ? (
+				<div
+					{...omitProps<IValidationProps>(errorChildProps, undefined)}
+					className={cx('&-error-content', errorChildProps.className)}
+				>
+					{errorChildProps.children}
+				</div>
+			) : null}
+		</div>
+	);
+};
+
+Validation.displayName = 'Validation';
+Validation.peek = {
+	description: `
+		Validation is a wrapper component that's meant to be used by other
+		components. Wrap your form components in it and style them accordingly
+		if there's an error.
+	`,
+	categories: ['helpers'],
+};
+Validation.propTypes = {
+	Error: any`
+		In most cases this will be a string, but it also accepts any valid React
+		element. If this is a falsey value, then no error message will be
+		displayed.  If this is the literal \`true\`, it will add the
+		\`-is-error\` class to the wrapper div, but not render the
+		\`-error-content\` \`div\`.
+	`,
+
+	className: string`
+		Classes that are appended to the component defaults. This prop is run
+		through the \`classnames\` library.
+	`,
+
+	children: any.isRequired`
+		Any valid React children.
+	`,
+};
+
+Validation.Error = ValidationError;
 
 export default Validation;
