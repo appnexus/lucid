10614c1b7e0b9d4a655b4eed71ea4ea379ed47fa
Dan Susskind <dan.susskind@xandr.com>
2019-08-14 17:32:38 -0700

    convert RadioButton to functional component

--- src/components/RadioButton/RadioButton.tsx
+++ src/components/RadioButton/RadioButton.tsx
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import React from 'react';
 import PropTypes from 'react-peek/prop-types';
 import { lucidClassNames } from '../../util/style-helpers';
-import { omitProps } from '../../util/component-types';
+import { omitProps, FC } from '../../util/component-types';
 
 const cx = lucidClassNames.bind('&-RadioButton');
 const { bool, func, object, string } = PropTypes;
@@ -30,7 +30,7 @@ interface IRadioButtonProps {
 	 * is in the unselected state.  Signature: \`(true, { event, props }) => {}\`
 	 */
 	onSelect?: (
-		selected: boolean,
+		isSelected: boolean,
 		{
 			event,
 			props,
@@ -44,135 +44,113 @@ interface IRadioButtonProps {
 	style?: React.CSSProperties;
 }
 
-class RadioButton extends React.Component<IRadioButtonProps, {}, {}> {
-	constructor(props: IRadioButtonProps) {
-		super(props);
-	}
+const RadioButton: FC<IRadioButtonProps> = (props): React.ReactElement => {
+	const {
+		className,
+		isDisabled = false,
+		isSelected = false,
+		onSelect = _.noop,
+		style,
+		...passThroughs
+	} = props;
 
-	private nativeElement = React.createRef<HTMLInputElement>();
+	const nativeElement = React.createRef<HTMLInputElement>();
 
-	static displayName = 'RadioButton';
-
-	static peek = {
-		description: `
-			This is a toggle -- a component that is in one of two particular states
-			at any given moment in time -- that features a custom visualization of
-			the native radio button control to reflect its current state.
-
-			The radio button is different from a standard toggle in that when it is
-			in the selected state user events do not cause it to toggle to the
-			unselected state so the \`onSelect\` function is called only when
-			\`isSelected\` is false.
-
-			It uses a hidden native radio button control under the hood but
-			leverages other HTML elements to visualize its state.
-
-			Any props that are not explicitly defined in \`propTypes\` are spread
-			onto the native control.
-			`,
-		categories: ['controls', 'toggles'],
-	};
-
-	static propTypes = {
-		className: string`
-			Appended to the component-specific class names set on the root element.
-		`,
-
-		isDisabled: bool`
-			Indicates whether the component should appear and act disabled by having
-			a "greyed out" palette and ignoring user interactions.
-		`,
-
-		isSelected: bool`
-			Indicates that the component is in the "selected" state when true and in
-			the "unselected" state when false.
-		`,
-
-		onSelect: func`
-			Called when the user clicks on the component or when they press the space
-			key while the component is in focus, and only called when the component
-			is in the unselected state.  Signature: \`(true, { event, props }) => {}\`
-		`,
-
-		style: object`
-			Passed through to the root element.
-		`,
-	};
-
-	static defaultProps = {
-		isDisabled: false,
-		isSelected: false,
-		onSelect: _.noop,
-	};
-
-	handleSpanClick = (e: React.MouseEvent<HTMLInputElement>): void => {
-		e.preventDefault();
-	};
-
-	render(): React.ReactNode {
-		const {
-			className,
-			isDisabled,
-			isSelected,
-			style,
-			...passThroughs
-		} = this.props;
-
-		return (
-			<span
-				className={cx(
-					'&',
-					{
-						'&-is-disabled': isDisabled,
-						'&-is-selected': isSelected,
-					},
-					className
-				)}
-				onClick={this.handleClicked}
-				style={style}
-			>
-				<input
-					onChange={_.noop}
-					{...omitProps(passThroughs, undefined, [
-						...Object.keys(RadioButton.propTypes),
-						'children',
-						'callbackId',
-					])}
-					checked={isSelected}
-					className={cx('&-native')}
-					disabled={isDisabled}
-					ref={this.nativeElement}
-					type='radio'
-				/>
-				<span
-					onClick={this.handleSpanClick}
-					className={cx('&-visualization-glow')}
-				/>
-				<span
-					onClick={this.handleSpanClick}
-					className={cx('&-visualization-container')}
-				/>
-				<span
-					onClick={this.handleSpanClick}
-					className={cx('&-visualization-dot')}
-				/>
-			</span>
-		);
-	}
-
-	handleClicked = (event: React.MouseEvent<HTMLInputElement>): void => {
-		const { isDisabled, isSelected, onSelect } = this.props;
-		const { current: element } = this.nativeElement;
-
+	function handleClicked(event: React.MouseEvent<HTMLInputElement>): void {
 		if (!isDisabled && !isSelected) {
-			if (onSelect) {
-				onSelect(true, { event, props: this.props });
-			}
-			if (element) {
-				element.focus();
+			onSelect(true, { event, props });
+			if (nativeElement.current) {
+				nativeElement.current.focus();
 			}
 		}
-	};
-}
+	}
+
+	function handleSpanClick(e: React.MouseEvent<HTMLInputElement>): void {
+		e.preventDefault();
+	}
+
+	return (
+		<span
+			className={cx(
+				'&',
+				{
+					'&-is-disabled': isDisabled,
+					'&-is-selected': isSelected,
+				},
+				className
+			)}
+			onClick={handleClicked}
+			style={style}
+		>
+			<input
+				onChange={_.noop}
+				{...omitProps(passThroughs, undefined, [
+					..._.keys(RadioButton.propTypes),
+					'children',
+					'callbackId',
+				])}
+				checked={isSelected}
+				className={cx('&-native')}
+				disabled={isDisabled}
+				ref={nativeElement}
+				type='radio'
+			/>
+			<span onClick={handleSpanClick} className={cx('&-visualization-glow')} />
+			<span
+				onClick={handleSpanClick}
+				className={cx('&-visualization-container')}
+			/>
+			<span onClick={handleSpanClick} className={cx('&-visualization-dot')} />
+		</span>
+	);
+};
+
+RadioButton.displayName = 'RadioButton';
+
+RadioButton.peek = {
+	description: `
+		This is a toggle -- a component that is in one of two particular states
+		at any given moment in time -- that features a custom visualization of
+		the native radio button control to reflect its current state.
+
+		The radio button is different from a standard toggle in that when it is
+		in the selected state user events do not cause it to toggle to the
+		unselected state so the \`onSelect\` function is called only when
+		\`isSelected\` is false.
+
+		It uses a hidden native radio button control under the hood but
+		leverages other HTML elements to visualize its state.
+
+		Any props that are not explicitly defined in \`propTypes\` are spread
+		onto the native control.
+		`,
+	categories: ['controls', 'toggles'],
+};
+
+RadioButton.propTypes = {
+	className: string`
+		Appended to the component-specific class names set on the root element.
+	`,
+
+	isDisabled: bool`
+		Indicates whether the component should appear and act disabled by having
+		a "greyed out" palette and ignoring user interactions.
+	`,
+
+	isSelected: bool`
+		Indicates that the component is in the "selected" state when true and in
+		the "unselected" state when false.
+	`,
+
+	onSelect: func`
+		Called when the user clicks on the component or when they press the space
+		key while the component is in focus, and only called when the component
+		is in the unselected state.  Signature: \`(true, { event, props }) => {}\`
+	`,
+
+	style: object`
+		Passed through to the root element.
+	`,
+};
 
 export default RadioButton;
