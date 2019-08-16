4bd172dd15dcf87ce599d80f91ba087ee43380d3
Dan Susskind <dan.susskind@xandr.com>
2019-08-16 12:05:09 -0700

    convert Icon to functional component

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import React from 'react';
 import PropTypes from 'react-peek/prop-types';
 import { lucidClassNames } from '../../util/style-helpers';
-import { omitProps } from '../../util/component-types';
+import { omitProps, FC } from '../../util/component-types';
 
 const cx = lucidClassNames.bind('&-Icon');
 
@@ -66,139 +66,121 @@ export interface IIconProps {
 	color?: Color;
 }
 
-class Icon extends React.Component<IIconProps, {}, {}> {
-	constructor(props: IIconProps) {
-		super(props);
-	}
-	private svgRef = React.createRef<SVGSVGElement>();
+const Icon: FC<IIconProps> = (props): React.ReactElement => {
+	const {
+		className,
+		children,
+		color = Color.ColorPrimary,
+		size = 16,
+		viewBox = '0 0 16 16',
+		aspectRatio = 'xMidYMid meet',
+		isClickable = false,
+		isDisabled = false,
+		onClick = _.noop,
+		onSelect = _.noop,
+		...passThroughs
+	} = props;
 
-	static displayName = 'Icon';
+	const svgRef = React.createRef<SVGSVGElement>();
 
-	static peek = {
-		description: `
-			A basic svg icon. Any props that are not explicitly called out below
-			will be passed through to the native \`svg\` component.
-		`,
-		categories: ['visual design', 'icons'],
-	};
+	function handleClick(event: React.MouseEvent): void {
+		onClick({ event, props: props });
 
-	static propTypes = {
-		className: any`
-			Classes that are appended to the component defaults. This prop is run
-			through the \`classnames\` library.
-		`,
-
-		size: number`
-			Size variations of the icons. \`size\` directly effects height and width
-			but the developer should also be conscious of the relationship with
-			\`viewBox\`.
-		`,
-
-		viewBox: string`
-			\`viewBox\` is very important for SVGs. You can think of \`viewBox\` as
-			the "artboard" for our SVG while \`size\` is the presented height and
-			width.
-		`,
-
-		aspectRatio: string`
-			Any valid SVG aspect ratio.
-		`,
-
-		isClickable: bool`
-			Adds styling that makes the icon appear clickable.
-		`,
-
-		isDisabled: bool`
-			Adds styling that makes the icon appear disabled.  Also forces
-			isClickable to be false.
-		`,
-
-		onClick: func`
-			Called when the user clicks the \`Icon\`. Signature:
-			\`({event, props}) => {}\`
-		`,
-
-		onSelect: func`
-			Called when the user clicks an active, clickable \`Icon\`. Signature:
-			\`({event, props}) => {}\`
-		`,
-
-		children: any`
-			Any valid React children.
-		`,
-
-		color: oneOf(_.values(Color))`
-			Sets the color of the Icon.  May not be applicable for icons that are tied
-			to specific colors (e.g. DangerIcon).
-		`,
-	};
-
-	static defaultProps = {
-		size: 16,
-		aspectRatio: 'xMidYMid meet',
-		viewBox: '0 0 16 16',
-		isDisabled: false,
-		isClickable: false,
-		color: Color.ColorPrimary,
-	};
-
-	handleClick = (event: React.MouseEvent): void => {
-		const { onClick, isDisabled, isClickable, onSelect } = this.props;
-
-		const domNode = this.svgRef.current;
-
-		if (onClick) {
-			onClick({ event, props: this.props });
-		}
-
-		if (onSelect && isClickable && !isDisabled) {
-			if (domNode) {
-				domNode.focus();
+		if (isClickable && !isDisabled) {
+			onSelect({ event, props: props });
+			if (svgRef.current) {
+				svgRef.current.focus();
 			}
-			onSelect({ event, props: this.props });
 		}
-	};
-
-	render(): React.ReactNode {
-		const {
-			className,
-			children,
-			color,
-			size,
-			viewBox,
-			aspectRatio,
-			isClickable,
-			isDisabled,
-			...passThroughs
-		} = this.props;
-
-		return (
-			<svg
-				width={size}
-				height={size}
-				viewBox={viewBox}
-				preserveAspectRatio={aspectRatio}
-				{...omitProps<IIconProps>(
-					passThroughs,
-					undefined,
-					Object.keys(Icon.propTypes)
-				)}
-				className={cx(
-					'&',
-					{
-						[`&-color-${color}`]: true,
-						'&-is-clickable': !isDisabled && isClickable,
-						'&-is-disabled': isDisabled,
-					},
-					className
-				)}
-				ref={this.svgRef}
-				onClick={this.handleClick}
-			>
-				{children}
-			</svg>
-		);
 	}
-}
+
+	return (
+		<svg
+			width={size}
+			height={size}
+			viewBox={viewBox}
+			preserveAspectRatio={aspectRatio}
+			{...omitProps<IIconProps>(
+				passThroughs,
+				undefined,
+				_.keys(Icon.propTypes)
+			)}
+			className={cx(
+				'&',
+				{
+					[`&-color-${color}`]: true,
+					'&-is-clickable': !isDisabled && isClickable,
+					'&-is-disabled': isDisabled,
+				},
+				className
+			)}
+			ref={svgRef}
+			onClick={handleClick}
+		>
+			{children}
+		</svg>
+	);
+};
+
+Icon.displayName = 'Icon';
+
+Icon.peek = {
+	description: `
+		A basic svg icon. Any props that are not explicitly called out below
+		will be passed through to the native \`svg\` component.
+	`,
+	categories: ['visual design', 'icons'],
+};
+
+Icon.propTypes = {
+	className: any`
+		Classes that are appended to the component defaults. This prop is run
+		through the \`classnames\` library.
+	`,
+
+	size: number`
+		Size variations of the icons. \`size\` directly effects height and width
+		but the developer should also be conscious of the relationship with
+		\`viewBox\`.
+	`,
+
+	viewBox: string`
+		\`viewBox\` is very important for SVGs. You can think of \`viewBox\` as
+		the "artboard" for our SVG while \`size\` is the presented height and
+		width.
+	`,
+
+	aspectRatio: string`
+		Any valid SVG aspect ratio.
+	`,
+
+	isClickable: bool`
+		Adds styling that makes the icon appear clickable.
+	`,
+
+	isDisabled: bool`
+		Adds styling that makes the icon appear disabled.  Also forces
+		isClickable to be false.
+	`,
+
+	onClick: func`
+		Called when the user clicks the \`Icon\`. Signature:
+		\`({event, props}) => {}\`
+	`,
+
+	onSelect: func`
+		Called when the user clicks an active, clickable \`Icon\`. Signature:
+		\`({event, props}) => {}\`
+	`,
+
+	children: any`
+		Any valid React children.
+	`,
+
+	color: oneOf(_.values(Color))`
+		Sets the color of the Icon.  May not be applicable for icons that are tied
+		to specific colors (e.g. DangerIcon).
+	`,
+};
 
 export default Icon;
