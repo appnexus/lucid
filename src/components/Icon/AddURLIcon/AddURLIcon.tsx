39f57477be77fb7e0430c5983fe3b913938d0fb7
Jack Toumey <jack@toumey.io>
2019-08-21 12:26:04 -0700

    first icon done

---
+++ src/components/Icon/AddURLIcon/AddURLIcon.tsx
@@ -0,0 +1,43 @@
+import _ from 'lodash';
+import React from 'react';
+import Icon from '../Icon';
+import { lucidClassNames } from '../../../util/style-helpers';
+import { FC } from '../../../util/component-types';
+import { IIconProps } from '../Icon';
+
+const cx = lucidClassNames.bind('&-AddURLIcon');
+
+interface IAddURLIconProps extends IIconProps {}
+
+const AddURLIcon: FC<IAddURLIconProps> = (props): React.ReactElement => {
+
+	const { className, ...passThroughs } = props;
+
+	return (
+		<Icon
+			{...passThroughs}
+			{..._.pick(passThroughs, _.keys(Icon.propTypes))}
+			className={cx('&', className)}
+		>
+			<path d='M10.7 12.781l1.267 2.719 2.357-1.098-1.267-2.719 2.693-.614L8.5 4.98l-.002 9.468z' />
+			<path d='M15.5.5H.5v10h6' />
+		</Icon>
+	);
+};
+
+AddURLIcon.displayName = 'AddURLIcon';
+
+	AddURLIcon.peek = {
+		description: `
+			Add URL
+		`,
+		categories: ['visual design', 'icons'],
+		extend: 'Icon',
+		madeFrom: ['Icon'],
+	};
+
+	AddURLIcon.propTypes = {
+		...Icon.propTypes,
+	};
+
+export default AddURLIcon;
