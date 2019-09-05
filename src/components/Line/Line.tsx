fa1594162e21b8b6f6abf4d2f8ffd04f25b700fd
Jack Toumey <jack@toumey.io>
2019-09-05 12:16:01 -0700

    Line/Lines converted

---
+++ src/components/Line/Line.tsx
@@ -0,0 +1,94 @@
+import _ from 'lodash';
+import React from 'react';
+import PropTypes from 'react-peek/prop-types';
+import { lucidClassNames } from '../../util/style-helpers';
+import { FC, omitProps, StandardProps } from '../../util/component-types';
+import * as chartConstants from '../../constants/charts';
+
+const cx = lucidClassNames.bind('&-Line');
+
+const { string, object, bool } = PropTypes;
+
+export interface ILineProps extends StandardProps {
+	/** The path for the line. */
+	d?: string;
+
+	/** Strings should match an existing color class unless they start with a '#' for specific colors. E.g.:
+
+		- \`COLOR_0\`
+		- \`COLOR_GOOD\`
+		- \`'#123abc'\` */
+	color?: string;
+
+	/** Display a dotted line. */
+	isDotted?: boolean;
+}
+
+const Line: FC<ILineProps> = ({
+	className,
+	color = chartConstants.COLOR_0,
+	isDotted = false,
+	d,
+	style,
+	...passThroughs
+}): React.ReactElement => {
+
+	const isCustomColor = _.startsWith(color, '#');
+	const colorStyle = isCustomColor ? { fill: color, stroke: color } : null;
+
+	return (
+		<path
+			{...omitProps<ILineProps>(
+				passThroughs,
+				undefined,
+				_.keys(Line.propTypes)
+			)}
+			style={{
+				...style,
+				...colorStyle,
+			}}
+			className={cx(className, '&', {
+				[`&-${color}`]: !isCustomColor,
+				'&-is-dotted': isDotted,
+			})}
+			d={d}
+		/>
+	);
+};
+
+Line.displayName = 'Line';
+Line.peek = {
+	description: `
+		*For use within an \`svg\`*
+
+		Lines are typically used for line charts and are pretty much a thin
+		wrapper around svg paths.
+	`,
+	categories: ['visualizations', 'geoms'],
+};
+Line.propTypes = {
+	style: object`
+		Passed through to the root element.
+	`,
+
+	className: string`
+		Appended to the component-specific class names set on the root element.
+	`,
+
+	d: string`
+		The path for the line.
+	`,
+
+	color: string`
+		Strings should match an existing color class unless they start with a '#' for specific colors. E.g.:
+
+		- \`COLOR_0\`
+		- \`COLOR_GOOD\`
+		- \`'#123abc'\`
+	`,
+	isDotted: bool`
+		Display a dotted line.
+	`,
+};
+
+export default Line;
