3e1a857bd105f504ffdc2d8a0f7e8838450fba82
Dan Susskind <dan.susskind@xandr.com>
2019-09-19 11:28:58 -0700

    upgrade storybook to add new Docs add-on

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,16 @@
-import { configure } from '@storybook/react';
+import { addParameters, configure } from '@storybook/react';
+import { DocsPage, DocsContainer } from '@storybook/addon-docs/blocks';
 import 'storybook-chromatic';
 
 function loadStories() {
 	require('../docs/index.stories.js');
 }
+
+addParameters({
+  docs: {
+    container: DocsContainer,
+    page: DocsPage,
+  },
+});
 
 configure(loadStories, module);
