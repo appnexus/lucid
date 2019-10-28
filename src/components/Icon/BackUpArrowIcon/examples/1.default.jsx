5b7f4f7345186a831dc7e80a49957f3c2860ca00
Dustin Reid <dustin.reid@xandr.com>
2019-10-28 12:57:56 -0700

    Added BackUpArrowIcon and updated snapshots

---
+++ src/components/Icon/BackUpArrowIcon/examples/1.default.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import createClass from 'create-react-class';
+import { BackUpArrowIcon } from '../../../../index';
+
+export default createClass({
+	render() {
+		return <BackUpArrowIcon />;
+	},
+});
