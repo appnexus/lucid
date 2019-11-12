b06093c21d541ddcc0c7afcbbfc69b957e1f6d67
Jon de la Motte <jon.de.la.motte@xandr.com>
2019-11-11 16:05:11 -0800

    Updated chart colors for certain kinds of color blindness

--- src/components/Legend/examples/basic.jsx
+++ src/components/Legend/examples/basic.jsx
@@ -1,138 +1,17 @@
-import _ from 'lodash';
 import React from 'react';
 import createClass from 'create-react-class';
 
-import { Legend, ToolTip, chartConstants } from '../../../index';
-
-const { Item } = Legend;
+import { Legend } from '../../../index';
 
 export default createClass({
 	render() {
 		return (
 			<div>
 				<Legend>
-					{_.map(chartConstants.PALETTE_7, (color, i) => (
-						<Item key={color} hasPoint color={color}>
-							{`Partner ${i}`}
-						</Item>
-					))}
+					<Legend.Item hasPoint={true} color='#ff0000'>
+						Hello
+					</Legend.Item>
 				</Legend>
-
-				<br />
-
-				<Legend>
-					{_.map(chartConstants.PALETTE_7, (color, i) => (
-						<Item key={color} hasPoint pointKind={i} color={color}>
-							{`Partner ${i}`}
-						</Item>
-					))}
-				</Legend>
-
-				<br />
-
-				<Legend>
-					{_.map(chartConstants.PALETTE_7, (color, i) => (
-						<Item key={color} hasLine color={color}>
-							{`Partner ${i}`}
-						</Item>
-					))}
-				</Legend>
-
-				<br />
-
-				<Legend>
-					{_.map(chartConstants.PALETTE_7, (color, i) => (
-						<Item key={color} hasPoint hasLine pointKind={i} color={color}>
-							{`Partner ${i}`}
-						</Item>
-					))}
-				</Legend>
-
-				<br />
-
-				<Legend isReversed>
-					{_.map(chartConstants.PALETTE_7, (color, i) => (
-						<Item key={color} hasPoint hasLine pointKind={i} color={color}>
-							{`Partner ${i}`}
-						</Item>
-					))}
-				</Legend>
-
-				<br />
-
-				<Legend orient='horizontal'>
-					<Item hasLine color={chartConstants.COLOR_GOOD}>
-						Revenue
-					</Item>
-					<Item hasLine color={chartConstants.COLOR_BAD}>
-						Loss
-					</Item>
-					<Item hasPoint color={chartConstants.COLOR_0}>
-						Partner 0
-					</Item>
-					<Item hasPoint color={chartConstants.COLOR_1}>
-						Partner 1
-					</Item>
-				</Legend>
-
-				<br />
-
-				<Legend orient='horizontal' isReversed>
-					<Item hasLine color={chartConstants.COLOR_GOOD}>
-						Revenue
-					</Item>
-					<Item hasLine color={chartConstants.COLOR_BAD}>
-						Loss
-					</Item>
-					<Item hasPoint color={chartConstants.COLOR_0}>
-						Partner 0
-					</Item>
-					<Item hasPoint color={chartConstants.COLOR_1}>
-						Partner 1
-					</Item>
-				</Legend>
-
-				<br />
-
-				<Legend>
-					<Item hasLine color={chartConstants.COLOR_GOOD}>
-						Revenue
-					</Item>
-					<Item hasLine color={chartConstants.COLOR_BAD}>
-						Loss
-					</Item>
-					<Item hasPoint color={chartConstants.COLOR_0}>
-						Partner 0
-					</Item>
-					<Item hasPoint color={chartConstants.COLOR_1}>
-						Partner 1
-					</Item>
-				</Legend>
-
-				<br />
-				<br />
-				<br />
-
-				<ToolTip isExpanded={true} direction='right' alignment='end'>
-					<ToolTip.Target>Tooltip example</ToolTip.Target>
-
-					<ToolTip.Body>
-						<Legend hasBorders={false}>
-							<Item hasLine color={chartConstants.COLOR_GOOD}>
-								Revenue
-							</Item>
-							<Item hasLine color={chartConstants.COLOR_BAD}>
-								Loss
-							</Item>
-							<Item hasPoint color={chartConstants.COLOR_0}>
-								Partner 0
-							</Item>
-							<Item hasPoint color={chartConstants.COLOR_1}>
-								Partner 1
-							</Item>
-						</Legend>
-					</ToolTip.Body>
-				</ToolTip>
 			</div>
 		);
 	},
--- src/components/Lines/examples/basic.jsx
+++ src/components/Lines/examples/basic.jsx
@@ -3,19 +3,19 @@ import React from 'react';
 import createClass from 'create-react-class';
 import { Lines, d3Scale } from '../../../index';
 
-const width = 1000;
+const width = 800;
 const height = 400;
 
 const data = [
-	{ x: 'one', y0: 1, y1: 2, y2: 3, y3: 5 },
-	{ x: 'two', y0: 2, y1: 3, y2: 4, y3: 6 },
-	{ x: 'three', y0: 2, y1: 4, y2: 5, y3: 6 },
-	{ x: 'four', y0: 3, y1: 6, y2: 7, y3: 7 },
-	{ x: 'five', y0: 4, y1: 8, y2: 9, y3: 8 },
-	{ x: 'six', y0: 20, y1: 8, y2: 9, y3: 1 },
+	{ x: 'one', y0: 20, y1: 18, y2: 16, y3: 14, y4: 12, y5: 10, y6: 8 },
+	{ x: 'two', y0: 2, y1: 3, y2: 4, y3: 6, y4: 8, y5: 9, y6: 6 },
+	{ x: 'three', y0: 2, y1: 4, y2: 5, y3: 6, y4: 9, y5: 10, y6: 3 },
+	{ x: 'four', y0: 3, y1: 6, y2: 7, y3: 7, y4: 7, y5: 4, y6: 2 },
+	{ x: 'five', y0: 4, y1: 8, y2: 9, y3: 8, y4: 5, y5: 4, y6: 3 },
+	{ x: 'six', y0: 20, y1: 18, y2: 16, y3: 14, y4: 12, y5: 10, y6: 8 },
 ];
 
-const yFields = ['y0', 'y1', 'y2', 'y3'];
+const yFields = ['y0', 'y1', 'y2', 'y3', 'y4', 'y5', 'y6'];
 const yMax = _.max(
 	_.reduce(
 		yFields,
