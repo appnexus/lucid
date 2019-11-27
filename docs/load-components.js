03128ce5965405f83b8c4aa9f9dc3ec47350016d
Dustin Reid <dustin.reid@xandr.com>
2019-11-27 15:54:46 -0800

    rename examples to .tsx

--- docs/load-components.js
+++ docs/load-components.js
@@ -14,12 +14,12 @@ module.exports = [
 		examplesContext: require.context(
 			'../src/components/Accordion/examples',
 			true,
-			/\.jsx?$/
+			/\.tsx?$/
 		),
 		examplesContextRaw: require.context(
 			'!!raw-loader!../src/components/Accordion/examples',
 			true,
-			/\.jsx?$/
+			/\.tsx?$/
 		),
 	},
 	{
