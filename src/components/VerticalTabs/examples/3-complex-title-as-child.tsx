9289f9ea9e00c9ebcf9f3d8d9fa4d133f98723a3
Noah Yasskin <noah.yasskin@xandr.com>
2019-12-09 15:49:04 -0800

    add failed attempts to resolve TS error

--- src/components/VerticalTabs/examples/3-complex-title-as-child.tsx
+++ src/components/VerticalTabs/examples/3-complex-title-as-child.tsx
@@ -18,14 +18,22 @@ const data = [
 	{ x: 6, y: 3 },
 ];
 
+// const data = [
+// 	{ x: '1', y: '1' },
+// 	{ x: '2', y: '0' },
+// 	{ x: '3', y: '3' },
+// 	{ x: '4', y: '2' },
+// 	{ x: '5', y: '1' },
+// 	{ x: '6', y: '3' },
+// ];
+
 const width = 200;
 const height = 50;
 const xScale = d3Scale
-	//const xScale: ScaleBand<ReactText> | ScalePoint<ReactText> = d3Scale
 	.scalePoint()
+	.domain(_.map(data, 'x'))
 	//.domain(_.map(data, datum => `${datum.x}`))
-	//.domain(_.map(data, 'x'))
-	.domain(['1', '2', '3', '4', '5', '6'])
+	//.domain(['1', '2', '3', '4', '5', '6'])
 	//.domain([1, 2, 3, 4, 5, 6])
 	.range([0, width]);
 
