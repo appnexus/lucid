75e476d0c5e6a95c13cdf56404bf7673bce02808
Noah Yasskin <noah.yasskin@xandr.com>
2019-12-09 11:34:37 -0800

    change example back to jsx

--- src/components/VerticalTabs/examples/3-complex-title-as-child.jsx
+++ src/components/VerticalTabs/examples/3-complex-title-as-child.jsx
@@ -10,19 +10,19 @@ import {
 } from '../../../index';
 
 const data = [
-	{ x: '1', y: 1 },
-	{ x: '2', y: 0 },
-	{ x: '3', y: 3 },
-	{ x: '4', y: 2 },
-	{ x: '5', y: 1 },
-	{ x: '6', y: 3 },
+	{ x: 1, y: 1 },
+	{ x: 2, y: 0 },
+	{ x: 3, y: 3 },
+	{ x: 4, y: 2 },
+	{ x: 5, y: 1 },
+	{ x: 6, y: 3 },
 ];
 const width = 200;
 const height = 50;
 const xScale = d3Scale
 	.scalePoint()
 	.domain(_.map(data, datum => `${datum.x}`))
-	//.domain(['0', '4'])
+	//.domain(_map(data, 'x'))
 	.range([0, width]);
 
 const yScale = d3Scale
