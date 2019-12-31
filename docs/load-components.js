a6ccd6cea571b9af7ba4e7eb53d9fb26787c31be
Noah Yasskin <noah.yasskin@xandr.com>
2019-12-31 15:11:23 -0800

    conver example to ts

--- docs/load-components.js
+++ docs/load-components.js
@@ -60,12 +60,12 @@ module.exports = [
 		examplesContext: require.context(
 			'../src/components/AxisLabel/examples',
 			true,
-			/\.jsx?$/
+			/\.tsx?$/
 		),
 		examplesContextRaw: require.context(
 			'!!raw-loader!../src/components/AxisLabel/examples',
 			true,
-			/\.jsx?$/
+			/\.tsx?$/
 		),
 	},
 	{
