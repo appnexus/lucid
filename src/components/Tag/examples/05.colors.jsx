d2189870b11d72278a887414cb5993a11835e558
gschnall <gschnall@xandr.com>
2020-01-07 10:41:37 -0800

    Removed unnecessary border colors from example and updated the snapshot for Tags.

--- src/components/Tag/examples/05.colors.jsx
+++ src/components/Tag/examples/05.colors.jsx
@@ -7,21 +7,13 @@ export default createClass({
 		return (
 			<div>
 				<Tag>
-					<Tag kind={'success'}>victoria</Tag>
 					<Tag kind={'info'}>notitia</Tag>
-					<Tag kind={'warning'}>monitum</Tag>
 					<Tag kind={'danger'}>periculum</Tag>
 					<Tag kind={'default'}>deficio</Tag>
 				</Tag>
 				<Tag>
-					<Tag isRemovable kind={'success'}>
-						victoria
-					</Tag>
 					<Tag isRemovable kind={'info'}>
 						notitia
-					</Tag>
-					<Tag isRemovable kind={'warning'}>
-						monitum
 					</Tag>
 					<Tag isRemovable kind={'danger'}>
 						periculum
