239489bdcd5bb85fc678e280301bb3c6b9331520
Noah Yasskin <noah.yasskin@xandr.com>
2020-01-10 14:41:02 -0800

    allow SingleSelect examples to be jsx or tsx

--- docs/load-components.js
+++ docs/load-components.js
@@ -854,12 +854,12 @@ module.exports = [
 		examplesContext: require.context(
 			'../src/components/SingleSelect/examples',
 			true,
-			/\.tsx?$/
+			/\.(j|t)sx?$/
 		),
 		examplesContextRaw: require.context(
 			'!!raw-loader!../src/components/SingleSelect/examples',
 			true,
-			/\.tsx?$/
+			/\.(j|t)sx?$/
 		),
 	},
 	{
