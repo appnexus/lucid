d0de168f5ca6bf5775d6700a406990ae0cd29aad
Xinxin Shi <xshi@appnexus.com>
2020-03-06 13:47:07 -0500

    test case update

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -498,7 +498,7 @@ export class Th extends React.Component<IThProps, IThState> {
 			passiveWidth = parseInt(passiveWidth);
 		}
 		
-		const activeWidth = ((this.props.minWidth && passiveWidth + coordinates.dX > this.props.minWidth) || !this.props.minWidth) ? passiveWidth + coordinates.dX : this.props.minWidth;
+		const activeWidth = passiveWidth + coordinates.dX;
 
 		this.setState({ activeWidth });
 
