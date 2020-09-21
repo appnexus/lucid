ec8f80455e3664ba9e5d135190261417824c3a6f
Noah Yasskin <noah.yasskin@xandr.com>
2020-09-21 14:02:24 -0700

    fix lightbulb color and update snapshot

--- src/components/Icon/LightbulbIcon/LightbulbIcon.tsx
+++ src/components/Icon/LightbulbIcon/LightbulbIcon.tsx
@@ -23,19 +23,15 @@ export const LightbulbIcon = ({
 			{..._.pick(passThroughs, _.keys(iconPropTypes))}
 			className={cx('&', className)}
 		>
-			<path d='M10 13.5H6M9 15.5H7' stroke='#000' />
+			<path d='M10 13.5H6M9 15.5H7' />
 			<path
 				id='a_1_'
 				d='M10 11.5h0c0-.788.617-1.562.995-1.845l.095-.075c1.261-.946 1.986-2.427 1.986-4.004C13.076 2.801 10.806.5 8 .5S2.924 2.801 2.924 5.576c0 1.576.725 3.058 1.986 4.004l.095.075C5.383 9.939 6 10.712 6 11.5h4z'
 				fillRule='evenodd'
 				clipRule='evenodd'
 				fill='none'
-				stroke='#000'
-				strokeWidth='1.3'
-				strokeLinecap='square'
-				strokeMiterlimit='10'
 			/>
-			<path d='M8 11.474V6M7 6h2' stroke='#000' />
+			<path d='M8 11.474V6M7 6h2' />
 		</Icon>
 	);
 };
