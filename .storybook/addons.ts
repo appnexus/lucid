2a7404c056720e7357aa3340ada6a34032fcf211
Tim Santeford <tsantef@gmail.com>
2021-03-09 22:48:04 -0800

    Fixed lint

--- .storybook/addons.ts
+++ .storybook/addons.ts
@@ -2,4 +2,4 @@ import '@storybook/addon-docs/register';
 import '@storybook/addon-notes/register-panel';
 import '@storybook/addon-options/register';
 import '@storybook/addon-links/register';
-require('./lucid-docs-addon/register');
+import './lucid-docs-addon/register';
