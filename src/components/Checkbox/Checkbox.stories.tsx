5664924ff72729ddc7267eba59ef573c732aaee1
Tim Santeford <tsantef@gmail.com>
2021-05-17 13:26:48 -0700

    Migrated Checkbox stories to Storybook 6

---
+++ src/components/Checkbox/Checkbox.stories.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { Meta } from '@storybook/react';
+import Checkbox from './Checkbox';
+
+export default {
+	title: 'Controls/Checkbox',
+	component: Checkbox,
+	parameters: {
+		notes: Checkbox.peek.description,
+	},
+} as Meta;
+
+const Template = (args) => {
+	const [selected, setSelected] = useState(args.isSelected || false);
+	return (
+		<Checkbox
+			{...args}
+			isSelected={selected}
+			onSelect={() => setSelected(!selected)}
+		/>
+	);
+};
+
+export const Default = Template.bind({});
+Default.args = {
+	title: 'Default',
+};
+
+export const Plain = Template.bind({});
+Plain.args = {
+	title: 'Plain',
+	isDisabled: false,
+	isSelected: true,
+};
+
+export const DisabledUnselected = Template.bind({});
+DisabledUnselected.args = {
+	title: 'Disabled Unselected',
+	isDisabled: true,
+	isSelected: false,
+};
+
+export const DisabledSelected = Template.bind({});
+DisabledSelected.args = {
+	title: 'Disabled Selected',
+	isDisabled: true,
+	isSelected: true,
+};
+
+export const DisabledIndeterminate = Template.bind({});
+DisabledIndeterminate.args = {
+	title: 'Disabled Selected',
+	isIndeterminate: true,
+	isDisabled: true,
+};
---
+++ src/components/CheckboxLabeled/CheckboxLabeled.stories.tsx
@@ -0,0 +1,99 @@
+import React, { useState } from 'react';
+import { Meta } from '@storybook/react';
+import CheckboxLabeled from './CheckboxLabeled';
+
+export default {
+	title: 'Controls/CheckboxLabeled',
+	component: CheckboxLabeled,
+	subcomponents: { 'CheckboxLabeled.Label': CheckboxLabeled.Label },
+	parameters: {
+		notes: CheckboxLabeled.peek.description,
+	},
+} as Meta;
+
+const Template = (args) => {
+	const [selected, setSelected] = useState(args.isSelected || false);
+	return (
+		<CheckboxLabeled
+			{...args}
+			isSelected={selected}
+			onSelect={() => setSelected(!selected)}
+		/>
+	);
+};
+
+export const Default = Template.bind({});
+Default.args = {
+	title: 'Default',
+	Label: 'Default',
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+	title: 'Disabled',
+	Label: 'Disabled',
+	isDisabled: true,
+};
+
+export const Selected = Template.bind({});
+Selected.args = {
+	title: 'Selected',
+	Label: 'Selected',
+	isSelected: true,
+};
+
+export const DisabledSelected = Template.bind({});
+DisabledSelected.args = {
+	title: 'Disabled Selected',
+	Label: 'Disabled Selected',
+	isDisabled: true,
+	isSelected: true,
+};
+
+export const LabelAsProp = Template.bind({});
+LabelAsProp.args = {
+	Label: 'Label as prop',
+	title: 'Label as prop',
+};
+
+export const HTMLElement = Template.bind({});
+HTMLElement.args = {
+	Label: <span>HTML element</span>,
+	title: 'HTML element',
+};
+
+export const TextInAnArray = Template.bind({});
+TextInAnArray.args = {
+	Label: [
+		'Text in an array',
+		'Only the first value in the array is used',
+		'The rest of these should be ignored',
+	],
+	title: 'Text in an array',
+};
+
+export const HTMLElementInAnArray = Template.bind({});
+HTMLElementInAnArray.args = {
+	Label: [
+		<span key='1'>HTML element in an array</span>,
+		<span key='2'>Again only the first value in the array is used</span>,
+		<span key='3'>The rest should not be rendered</span>,
+	],
+	title: 'HTML element in an array',
+};
+
+export const LabelAsChild = (args) => {
+	const [selected, setSelected] = useState(args.isSelected || false);
+
+	return (
+		<CheckboxLabeled
+			{...args}
+			isSelected={selected}
+			onSelect={() => setSelected(!selected)}
+		>
+			<CheckboxLabeled.Label>
+				<span>HTML element as Child</span>
+			</CheckboxLabeled.Label>
+		</CheckboxLabeled>
+	);
+};
