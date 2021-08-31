79e6a88aa7a088aa710f61419bcf703d172dbb4f
Lennox David <lennox.david@xandr.com>
2021-08-31 15:19:08 -0400

    refactor: update backuparrowicon, pinicon and props order

--- src/components/Icon/PinIcon/PinIcon.spec.tsx
+++ src/components/Icon/PinIcon/PinIcon.spec.tsx
@@ -1,25 +1,8 @@
-import React from 'react';
-import { shallow } from 'enzyme';
 import { icons, common } from '../../../util/generic-tests';
-import assert from 'assert';
 
 import PinIcon from './PinIcon';
 
 describe('PinIcon', () => {
 	common(PinIcon);
 	icons(PinIcon);
-
-	describe('`direction` prop', () => {
-		it('should render the correct icon with "left"', () => {
-			const wrapper = shallow(<PinIcon direction='left' />);
-
-			assert.equal(wrapper.find('.lucid-PinIcon-is-left').length, 1);
-		});
-
-		it('should render the correct icon with "right"', () => {
-			const wrapper = shallow(<PinIcon direction='right' />);
-
-			assert.equal(wrapper.find('.lucid-PinIcon-is-right').length, 1);
-		});
-	});
 });
