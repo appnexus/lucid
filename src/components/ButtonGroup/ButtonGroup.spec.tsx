7f750556e6c16613b1f943dcfc606c1b3d181982
Noah Yasskin <noah.yasskin@xandr.com>
2022-02-28 16:50:44 -0800

    cleanup ButtonGroup unit test

--- src/components/ButtonGroup/ButtonGroup.spec.tsx
+++ src/components/ButtonGroup/ButtonGroup.spec.tsx
@@ -12,88 +12,88 @@ const defaultProps = ButtonGroup.defaultProps;
 describe('ButtonGroup', () => {
 	common(ButtonGroup);
 
-	it('prop children', () => {
-		const wrapper = shallow(
-			<ButtonGroup {...defaultProps}>
-				<ButtonGroup.Button data-test-name='one' />
-				<div className='jim' />
-			</ButtonGroup>
-		);
+	describe('props', () => {
+		it('prop children', () => {
+			const wrapper = shallow(
+				<ButtonGroup {...defaultProps}>
+					<ButtonGroup.Button data-test-name='one' />
+					<div className='jim' />
+				</ButtonGroup>
+			);
 
-		assert(wrapper.contains(<div className='jim' />));
-	});
+			assert(wrapper.contains(<div className='jim' />));
+		});
 
-	it('prop selectedIndices', () => {
-		const wrapper = shallow(
-			<ButtonGroup {...defaultProps} selectedIndices={[1]}>
-				<ButtonGroup.Button data-test-name='zero' />
-				<ButtonGroup.Button data-test-name='one' />
-			</ButtonGroup>
-		);
+		it('prop selectedIndices', () => {
+			const wrapper = shallow(
+				<ButtonGroup {...defaultProps} selectedIndices={[1]}>
+					<ButtonGroup.Button data-test-name='zero' />
+					<ButtonGroup.Button data-test-name='one' />
+				</ButtonGroup>
+			);
 
-		assert.strictEqual(wrapper.children().at(1).prop('isActive'), true);
-	});
-});
+			assert.strictEqual(wrapper.children().at(1).prop('isActive'), true);
+		});
 
-describe('ButtonGroup', () => {
-	it('prop onSelect', () => {
-		const onSelect: any = sinon.spy();
-		const wrapper = mount(
-			<ButtonGroup {...defaultProps} onSelect={onSelect}>
-				<ButtonGroup.Button data-test-name='zero' />
-				<ButtonGroup.Button data-test-name='one' />
-			</ButtonGroup>
-		);
+		it('prop onSelect', () => {
+			const onSelect: any = sinon.spy();
+			const wrapper = mount(
+				<ButtonGroup {...defaultProps} onSelect={onSelect}>
+					<ButtonGroup.Button data-test-name='zero' />
+					<ButtonGroup.Button data-test-name='one' />
+				</ButtonGroup>
+			);
 
-		wrapper.find('button[data-test-name="one"]').simulate('click');
+			wrapper.find('button[data-test-name="one"]').simulate('click');
 
-		assert.strictEqual(onSelect.args[0][0], 1);
-	});
+			assert.strictEqual(onSelect.args[0][0], 1);
+		});
 
-	it('prop onSelect on children', () => {
-		const onClick = sinon.spy();
-		const wrapper = mount(
-			<ButtonGroup {...defaultProps}>
-				<ButtonGroup.Button data-test-name='zero' />
-				<ButtonGroup.Button onClick={onClick} data-test-name='one' />
-			</ButtonGroup>
-		);
+		it('prop onSelect on children', () => {
+			const onClick = sinon.spy();
+			const wrapper = mount(
+				<ButtonGroup {...defaultProps}>
+					<ButtonGroup.Button data-test-name='zero' />
+					<ButtonGroup.Button onClick={onClick} data-test-name='one' />
+				</ButtonGroup>
+			);
 
-		wrapper.find('button[data-test-name="one"]').simulate('click');
+			wrapper.find('button[data-test-name="one"]').simulate('click');
 
-		assert(onClick.called, 'onClick was not called');
-		assert(
-			_.has(onClick.args[0][0], 'event'),
-			'`event` missing from `onClick` callback'
-		);
-		assert(
-			_.has(onClick.args[0][0], 'props'),
-			'`props` missing from `onClick` callback'
-		);
-	});
+			assert(onClick.called, 'onClick was not called');
+			assert(
+				_.has(onClick.args[0][0], 'event'),
+				'`event` missing from `onClick` callback'
+			);
+			assert(
+				_.has(onClick.args[0][0], 'props'),
+				'`props` missing from `onClick` callback'
+			);
+		});
 
-	it('tests onSelect props shape', () => {
-		const onSelect: any = jest.fn();
-		const wrapper = mount(
-			<ButtonGroup {...defaultProps} onSelect={onSelect}>
-				<ButtonGroup.Button data-test-name='zero' />
-				<ButtonGroup.Button data-test-name='one' />
-				<ButtonGroup.Button data-test-name='two' />
-			</ButtonGroup>
-		);
+		it('tests onSelect props have the correct shape', () => {
+			const onSelect: any = jest.fn();
+			const wrapper = mount(
+				<ButtonGroup {...defaultProps} onSelect={onSelect}>
+					<ButtonGroup.Button data-test-name='zero' />
+					<ButtonGroup.Button data-test-name='one' />
+					<ButtonGroup.Button data-test-name='two' />
+				</ButtonGroup>
+			);
 
-		wrapper.find('button[data-test-name="one"]').simulate('click');
+			wrapper.find('button[data-test-name="one"]').simulate('click');
 
-		expect(onSelect).toBeCalledWith(1, {
-			event: expect.anything(),
-			props: {
-				'data-test-name': 'one',
-				hasOnlyIcon: false,
-				isActive: false,
-				isDisabled: false,
-				onClick: expect.anything(),
-				type: 'button',
-			},
+			expect(onSelect).toBeCalledWith(1, {
+				event: expect.anything(),
+				props: {
+					'data-test-name': 'one',
+					hasOnlyIcon: false,
+					isActive: false,
+					isDisabled: false,
+					onClick: expect.anything(),
+					type: 'button',
+				},
+			});
 		});
 	});
 });
