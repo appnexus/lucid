8dfc39ec757e8eedeb89d5383f9e684858eea5f2
Noah Yasskin <noah.yasskin@xandr.com>
2022-04-11 16:35:00 -0700

    PieChart Stories new Type defs and make functional

--- src/components/PieChart/PieChart.stories.tsx
+++ src/components/PieChart/PieChart.stories.tsx
@@ -1,8 +1,10 @@
+import _ from 'lodash';
 import React from 'react';
 import createClass from 'create-react-class';
-import _ from 'lodash';
+import { Meta, Story } from '@storybook/react';
+
 import * as chartConstants from '../../constants/charts';
-import PieChart from './PieChart';
+import PieChart, { IPieChartProps } from './PieChart';
 import Legend from '../Legend/Legend';
 
 export default {
@@ -11,16 +13,15 @@ export default {
 	parameters: {
 		docs: {
 			description: {
-				component: (PieChart as any).peek.description,
+				component: PieChart.peek.description,
 			},
 		},
 	},
-};
+	args: PieChart.defaultProps,
+} as Meta;
 
 /* Basic */
-export const Basic = () => {
-	/* eslint-disable comma-spacing */
-
+export const Basic: Story<IPieChartProps> = (args) => {
 	const data = [
 		{ x: 'Leslie', y: 60 },
 		{ x: 'Ron', y: 40 },
@@ -33,45 +34,45 @@ export const Basic = () => {
 		paddingTop: '4rem',
 	};
 
-	const Component = createClass({
-		render() {
-			return (
-				<div style={style}>
-					<PieChart data={data} />
-					<PieChart
-						data={data}
-						palette={chartConstants.PALETTE_MONOCHROME_0_5}
-					/>
-					<PieChart
-						data={data}
-						palette={chartConstants.PALETTE_MONOCHROME_1_5}
-					/>
-					<PieChart
-						data={data}
-						palette={chartConstants.PALETTE_MONOCHROME_2_5}
-					/>
-					<PieChart
-						data={data}
-						palette={chartConstants.PALETTE_MONOCHROME_3_5}
-					/>
-					<PieChart
-						data={data}
-						palette={chartConstants.PALETTE_MONOCHROME_4_5}
-					/>
-					<PieChart
-						data={data}
-						palette={chartConstants.PALETTE_MONOCHROME_5_5}
-					/>
-				</div>
-			);
-		},
-	});
-
-	return <Component />;
+	return (
+		<section style={style}>
+			<PieChart data={data} />
+			<PieChart
+				data={data}
+				{...args}
+				palette={chartConstants.PALETTE_MONOCHROME_0_5}
+			/>
+			<PieChart
+				{...args}
+				data={data}
+				palette={chartConstants.PALETTE_MONOCHROME_1_5}
+			/>
+			<PieChart
+				{...args}
+				data={data}
+				palette={chartConstants.PALETTE_MONOCHROME_2_5}
+			/>
+			<PieChart
+				{...args}
+				data={data}
+				palette={chartConstants.PALETTE_MONOCHROME_3_5}
+			/>
+			<PieChart
+				{...args}
+				data={data}
+				palette={chartConstants.PALETTE_MONOCHROME_4_5}
+			/>
+			<PieChart
+				{...args}
+				data={data}
+				palette={chartConstants.PALETTE_MONOCHROME_5_5}
+			/>
+		</section>
+	);
 };
 
 /* Basic Donuts */
-export const BasicDonuts = () => {
+export const BasicDonuts: Story<IPieChartProps> = (args) => {
 	/* eslint-disable comma-spacing */
 
 	const data = [
@@ -86,52 +87,51 @@ export const BasicDonuts = () => {
 		paddingTop: '4rem',
 	};
 
-	const Component = createClass({
-		render() {
-			return (
-				<div style={style}>
-					<PieChart data={data} isDonut />
-					<PieChart
-						data={data}
-						palette={chartConstants.PALETTE_MONOCHROME_0_5}
-						isDonut
-					/>
-					<PieChart
-						data={data}
-						palette={chartConstants.PALETTE_MONOCHROME_1_5}
-						isDonut
-					/>
-					<PieChart
-						data={data}
-						palette={chartConstants.PALETTE_MONOCHROME_2_5}
-						isDonut
-					/>
-					<PieChart
-						data={data}
-						palette={chartConstants.PALETTE_MONOCHROME_3_5}
-						isDonut
-					/>
-					<PieChart
-						data={data}
-						palette={chartConstants.PALETTE_MONOCHROME_4_5}
-						isDonut
-					/>
-					<PieChart
-						data={data}
-						palette={chartConstants.PALETTE_MONOCHROME_5_5}
-						isDonut
-					/>
-				</div>
-			);
-		},
-	});
-
-	return <Component />;
+	return (
+		<section style={style}>
+			<PieChart {...args} data={data} isDonut />
+			<PieChart
+				{...args}
+				data={data}
+				palette={chartConstants.PALETTE_MONOCHROME_0_5}
+				isDonut
+			/>
+			<PieChart
+				{...args}
+				data={data}
+				palette={chartConstants.PALETTE_MONOCHROME_1_5}
+				isDonut
+			/>
+			<PieChart
+				{...args}
+				data={data}
+				palette={chartConstants.PALETTE_MONOCHROME_2_5}
+				isDonut
+			/>
+			<PieChart
+				{...args}
+				data={data}
+				palette={chartConstants.PALETTE_MONOCHROME_3_5}
+				isDonut
+			/>
+			<PieChart
+				{...args}
+				data={data}
+				palette={chartConstants.PALETTE_MONOCHROME_4_5}
+				isDonut
+			/>
+			<PieChart
+				{...args}
+				data={data}
+				palette={chartConstants.PALETTE_MONOCHROME_5_5}
+				isDonut
+			/>
+		</section>
+	);
 };
-BasicDonuts.storyName = 'BasicDonuts';
 
 /* Legend */
-export const LegendTest = () => {
+export const LegendTest: Story<IPieChartProps> = (args) => {
 	const data = [
 		{ x: 'Leslie', y: 80 },
 		{ x: 'Tom', y: 20 },
@@ -147,34 +147,27 @@ export const LegendTest = () => {
 		paddingTop: '4rem',
 	};
 
-	const Component = createClass({
-		render() {
-			return (
-				<div style={style}>
-					<PieChart data={data} palette={palette} />
-					<Legend>
-						{_.map(data, (d, index) => (
-							<Legend.Item
-								key={index}
-								color={palette[index % palette.length]}
-								hasPoint
-								pointKind={1}
-							>
-								{d.x}
-							</Legend.Item>
-						))}
-					</Legend>
-				</div>
-			);
-		},
-	});
-
-	return <Component />;
+	return (
+		<section style={style}>
+			<PieChart {...args} data={data} palette={palette} />
+			<Legend>
+				{_.map(data, (d, index) => (
+					<Legend.Item
+						key={index}
+						color={palette[index % palette.length]}
+						hasPoint
+						pointKind={1}
+					>
+						{d.x}
+					</Legend.Item>
+				))}
+			</Legend>
+		</section>
+	);
 };
-LegendTest.storyName = 'Legend';
 
 /* Color Map */
-export const ColorMap = () => {
+export const ColorMap: Story<IPieChartProps> = (args) => {
 	/* eslint-disable comma-spacing */
 
 	const data = [
@@ -189,28 +182,22 @@ export const ColorMap = () => {
 		paddingTop: '4rem',
 	};
 
-	const Component = createClass({
-		render() {
-			return (
-				<div style={style}>
-					<PieChart
-						data={data}
-						colorMap={{
-							Tammy: chartConstants.COLOR_BAD,
-							Leslie: chartConstants.COLOR_GOOD,
-						}}
-					/>
-				</div>
-			);
-		},
-	});
-
-	return <Component />;
+	return (
+		<section style={style}>
+			<PieChart
+				{...args}
+				data={data}
+				colorMap={{
+					Tammy: chartConstants.COLOR_BAD,
+					Leslie: chartConstants.COLOR_GOOD,
+				}}
+			/>
+		</section>
+	);
 };
-ColorMap.storyName = 'ColorMap';
 
 /* Percents */
-export const Percents = () => {
+export const Percents: Story<IPieChartProps> = (args) => {
 	/* eslint-disable comma-spacing */
 
 	const data = [
@@ -227,28 +214,21 @@ export const Percents = () => {
 		paddingTop: '4rem',
 	};
 
-	const Component = createClass({
-		render() {
-			return (
-				<div style={style}>
-					<PieChart
-						data={data}
-						yAxisFormatter={(value: number) => {
-							return `${((value / total) * 100).toFixed(1)}%`;
-						}}
-					/>
-				</div>
-			);
-		},
-	});
-
-	return <Component />;
+	return (
+		<section style={style}>
+			<PieChart
+				{...args}
+				data={data}
+				yAxisFormatter={(value: number) => {
+					return `${((value / total) * 100).toFixed(1)}%`;
+				}}
+			/>
+		</section>
+	);
 };
 
 /* Small With No Stroke Or Hover */
-export const SmallWithNoStrokeOrHover = () => {
-	/* eslint-disable comma-spacing */
-
+export const SmallWithNoStrokeOrHover: Story<IPieChartProps> = (args) => {
 	const data = [
 		{ x: 'Leslie', y: 60 },
 		{ x: 'Ron', y: 40 },
@@ -261,28 +241,22 @@ export const SmallWithNoStrokeOrHover = () => {
 		paddingTop: '4rem',
 	};
 
-	const Component = createClass({
-		render() {
-			return (
-				<div style={style}>
-					<PieChart
-						margin={{
-							top: 0,
-							right: 0,
-							bottom: 0,
-							left: 0,
-						}}
-						width={25}
-						height={25}
-						data={data}
-						hasStroke={false}
-						isHovering={false}
-					/>
-				</div>
-			);
-		},
-	});
-
-	return <Component />;
+	return (
+		<section style={style}>
+			<PieChart
+				{...args}
+				margin={{
+					top: 0,
+					right: 0,
+					bottom: 0,
+					left: 0,
+				}}
+				width={25}
+				height={25}
+				data={data}
+				hasStroke={false}
+				isHovering={false}
+			/>
+		</section>
+	);
 };
-SmallWithNoStrokeOrHover.storyName = 'SmallWithNoStrokeOrHover';
