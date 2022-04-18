41948fb201c020ad58969e8b1a76715323b9f882
Noah Yasskin <noah.yasskin@xandr.com>
2022-04-18 16:30:43 -0700

    Update stories to SB6

--- src/components/SplitButton/SplitButton.stories.tsx
+++ src/components/SplitButton/SplitButton.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import createClass from 'create-react-class';
-import SplitButton from './SplitButton';
+import { Meta, Story } from '@storybook/react';
+
+import SplitButton, { ISplitButtonProps } from './SplitButton';
 
 export default {
 	title: 'Controls/SplitButton',
@@ -8,110 +9,87 @@ export default {
 	parameters: {
 		docs: {
 			description: {
-				component: (SplitButton as any).peek.description,
+				component: SplitButton.peek.description,
 			},
 		},
 	},
-};
+	args: SplitButton.defaultProps,
+} as Meta;
 
 /* Basic */
-export const Basic = () => {
+export const Basic: Story<ISplitButtonProps> = (args) => {
 	const style = { marginRight: '20px', height: 100 };
 
-	const Component = createClass({
-		render() {
-			return (
-				<div>
-					<SplitButton style={style}>
-						<SplitButton.Button>Basic</SplitButton.Button>
-						<SplitButton.Button>One</SplitButton.Button>
-						<SplitButton.Button>Two</SplitButton.Button>
-					</SplitButton>
+	return (
+		<section>
+			<SplitButton {...args} style={style}>
+				<SplitButton.Button>Basic</SplitButton.Button>
+				<SplitButton.Button>One</SplitButton.Button>
+				<SplitButton.Button>Two</SplitButton.Button>
+			</SplitButton>
 
-					<SplitButton kind='primary' style={style}>
-						<SplitButton.Button>Primary</SplitButton.Button>
-						<SplitButton.Button>One</SplitButton.Button>
-						<SplitButton.Button>Two</SplitButton.Button>
-					</SplitButton>
-				</div>
-			);
-		},
-	});
-
-	return <Component />;
+			<SplitButton {...args} kind='primary' style={style}>
+				<SplitButton.Button>Primary</SplitButton.Button>
+				<SplitButton.Button>One</SplitButton.Button>
+				<SplitButton.Button>Two</SplitButton.Button>
+			</SplitButton>
+		</section>
+	);
 };
 
 /* Disabled */
-export const Disabled = () => {
-	const Component = createClass({
-		render() {
-			return (
-				<div style={{ height: 100 }}>
-					<SplitButton>
-						<SplitButton.Button isDisabled>Save</SplitButton.Button>
-						<SplitButton.Button isDisabled>
-							This action should be disabled
-						</SplitButton.Button>
-						<SplitButton.Button>This one should be enabled</SplitButton.Button>
-						<SplitButton.Button isDisabled>
-							This should be disabled, too
-						</SplitButton.Button>
-					</SplitButton>
-				</div>
-			);
-		},
-	});
-
-	return <Component />;
+export const Disabled: Story<ISplitButtonProps> = (args) => {
+	return (
+		<section style={{ height: 100 }}>
+			<SplitButton {...args}>
+				<SplitButton.Button isDisabled>Save</SplitButton.Button>
+				<SplitButton.Button isDisabled>
+					This action should be disabled
+				</SplitButton.Button>
+				<SplitButton.Button>This one should be enabled</SplitButton.Button>
+				<SplitButton.Button isDisabled>
+					This should be disabled, too
+				</SplitButton.Button>
+			</SplitButton>
+		</section>
+	);
 };
 
 /* Up */
-export const Up = () => {
-	const Component = createClass({
-		render() {
-			return (
-				<div style={{ height: 100 }}>
-					<SplitButton direction='up' kind='primary'>
-						<SplitButton.Button>Save</SplitButton.Button>
-						<SplitButton.Button>Action 01</SplitButton.Button>
-						<SplitButton.Button>Here's Another Action</SplitButton.Button>
-						<SplitButton.Button>And Another Action</SplitButton.Button>
-					</SplitButton>
-				</div>
-			);
-		},
-	});
-
-	return <Component />;
+export const Up: Story<ISplitButtonProps> = (args) => {
+	return (
+		<section style={{ height: 100 }}>
+			<SplitButton {...args} direction='up' kind='primary'>
+				<SplitButton.Button>Save</SplitButton.Button>
+				<SplitButton.Button>Action 01</SplitButton.Button>
+				<SplitButton.Button>Here's Another Action</SplitButton.Button>
+				<SplitButton.Button>And Another Action</SplitButton.Button>
+			</SplitButton>
+		</section>
+	);
 };
 
 /* Sizes */
-export const Sizes = () => {
+export const Sizes: Story<ISplitButtonProps> = (args) => {
 	const style = { marginRight: '20px', height: 100 };
 
-	const Component = createClass({
-		render() {
-			return (
-				<div>
-					<SplitButton size='large' style={style}>
-						<SplitButton.Button>Large</SplitButton.Button>
-						<SplitButton.Button>One</SplitButton.Button>
-						<SplitButton.Button>Two</SplitButton.Button>
-					</SplitButton>
-					<SplitButton size='small' style={style}>
-						<SplitButton.Button>Small</SplitButton.Button>
-						<SplitButton.Button>One</SplitButton.Button>
-						<SplitButton.Button>Two</SplitButton.Button>
-					</SplitButton>
-					<SplitButton size='short' style={style}>
-						<SplitButton.Button>Short</SplitButton.Button>
-						<SplitButton.Button>One</SplitButton.Button>
-						<SplitButton.Button>Two</SplitButton.Button>
-					</SplitButton>
-				</div>
-			);
-		},
-	});
-
-	return <Component />;
+	return (
+		<section>
+			<SplitButton {...args} size='large' style={style}>
+				<SplitButton.Button>Large</SplitButton.Button>
+				<SplitButton.Button>One</SplitButton.Button>
+				<SplitButton.Button>Two</SplitButton.Button>
+			</SplitButton>
+			<SplitButton {...args} size='small' style={style}>
+				<SplitButton.Button>Small</SplitButton.Button>
+				<SplitButton.Button>One</SplitButton.Button>
+				<SplitButton.Button>Two</SplitButton.Button>
+			</SplitButton>
+			<SplitButton {...args} size='short' style={style}>
+				<SplitButton.Button>Short</SplitButton.Button>
+				<SplitButton.Button>One</SplitButton.Button>
+				<SplitButton.Button>Two</SplitButton.Button>
+			</SplitButton>
+		</section>
+	);
 };
